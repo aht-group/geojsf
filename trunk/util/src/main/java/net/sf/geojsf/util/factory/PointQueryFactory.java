9439420c1c2f44c79eae0fc450df47a10d0239f4
thorsten-k <thorsten-k@d7d7bd7b-d12e-478d-8a14-7487b8a8350c>
2011-10-26 07:59:54 +0000

    factories added

---
+++ trunk/util/src/main/java/net/sf/geojsf/util/factory/PointQueryFactory.java
@@ -0,0 +1,98 @@
+package net.sf.geojsf.util.factory;
+
+import java.util.List;
+
+import net.sf.geojsf.xml.gml.Coordinates;
+import net.sf.geojsf.xml.gml.Point;
+import net.sf.geojsf.xml.ogc.DWithin;
+import net.sf.geojsf.xml.ogc.Distance;
+import net.sf.geojsf.xml.ogc.Filter;
+import net.sf.geojsf.xml.ogc.PropertyName;
+import net.sf.geojsf.xml.wfs.GetFeature;
+import net.sf.geojsf.xml.wfs.Query;
+
+public class PointQueryFactory
+{	
+	private Coordinates coordinates;
+	private Distance distance;
+	
+	public void setDistance(Distance distance) {this.distance = distance;}
+	public void setCoordinates(Coordinates coordinates) {this.coordinates = coordinates;}
+
+	public PointQueryFactory()
+	{
+		
+	}
+	
+	public GetFeature createGetFeature(String typeName, List<String> properties)
+	{
+		GetFeature gFeature = new GetFeature();
+		gFeature.setService("WFS");
+		gFeature.setVersion("1.0.0");
+		gFeature.setOutputFormat("GML2");
+		
+		gFeature.setQuery(createQuery(typeName,properties));
+		
+		return gFeature;
+	}
+	
+
+    /**
+     * @depreciated
+     */	
+	public static GetFeature cGetFeature(String typeName, List<String> properties, Coordinates coordinates, Distance distance)
+	{
+		PointQueryFactory pwf = new PointQueryFactory();
+		pwf.setCoordinates(coordinates);
+		pwf.setDistance(distance);
+		return pwf.createGetFeature(typeName, properties);
+	}
+	
+	private Query createQuery(String typeName, List<String> properties)
+	{
+		Query q = new Query();
+		q.setTypeName(typeName);
+		
+		for(String s : properties)
+		{
+			PropertyName pn = new PropertyName();
+			pn.setValue(s);
+			q.getPropertyName().add(pn);
+		}
+		
+		q.setFilter(createFilter());
+		
+		return q;
+	}
+	
+	private Filter createFilter()
+	{
+		Filter filter = new Filter();
+		filter.setDWithin(createDWithin(distance));
+		return filter;
+	}
+	
+	private DWithin createDWithin(Distance distance)
+	{
+		DWithin dw = new DWithin();
+		
+		PropertyName pn = new PropertyName();
+		pn.setValue("the_geom");
+		dw.setPropertyName(pn);
+		
+		dw.setPoint(createPoint());
+		dw.setDistance(distance);
+		
+		return dw;
+	}
+	
+	private Point createPoint()
+	{
+		Point point = new Point();
+		
+		point.setSrsName("http://www.opengis.net/gml/srs/epsg.xml#4326");
+		point.setCoordinates(coordinates);
+		
+		return point;
+	}
+}
