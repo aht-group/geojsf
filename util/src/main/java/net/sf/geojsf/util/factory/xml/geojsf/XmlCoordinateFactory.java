2da3fcee567be4075f77a43697d6ef0cf2178268
thorsten-k <t.kisner@web.de>
2012-06-25 13:21:06 +0000

    XmlCoordinateFactory added

---
+++ util/src/main/java/net/sf/geojsf/util/factory/xml/geojsf/XmlCoordinateFactory.java
@@ -0,0 +1,27 @@
+package net.sf.geojsf.util.factory.xml.geojsf;
+
+import java.io.Serializable;
+
+import net.sf.geojsf.xml.geojsf.Coordinate;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class XmlCoordinateFactory implements Serializable
+{
+	final static Logger logger = LoggerFactory.getLogger(XmlCoordinateFactory.class);
+	
+	public static final long serialVersionUID=1;
+	
+	// >>>>>>>>>>>>>>>>>>>>>>>>>>Fields<<<<<<<<<<<<<<<<<<<<<<<<<<<	
+
+	public static Coordinate build(double lat, double lon)
+	{
+		Coordinate xml = new Coordinate();
+		
+		xml.setLat(lat);
+		xml.setLon(lon);
+		
+		return xml;
+	}
+}
