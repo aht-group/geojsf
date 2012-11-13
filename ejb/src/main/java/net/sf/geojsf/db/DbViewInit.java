ddfb19fe9d21412c9c20609581923f2d2d81393c
thorsten-k <t.kisner@web.de>
2012-11-13 08:43:32 +0000

    xml:langs added to view (GEO-20)

--- ejb/src/main/java/net/sf/geojsf/db/DbViewInit.java
+++ ejb/src/main/java/net/sf/geojsf/db/DbViewInit.java
@@ -12,27 +12,32 @@ import net.sf.ahtutils.exception.processing.UtilsConfigurationException;
 import net.sf.ahtutils.model.interfaces.status.UtilsDescription;
 import net.sf.ahtutils.model.interfaces.status.UtilsLang;
 import net.sf.geojsf.model.interfaces.openlayers.GeoJsfLayer;
+import net.sf.geojsf.model.interfaces.openlayers.GeoJsfView;
 import net.sf.geojsf.xml.openlayers.Layer;
-import net.sf.geojsf.xml.openlayers.Layers;
+import net.sf.geojsf.xml.openlayers.View;
+import net.sf.geojsf.xml.openlayers.Views;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class DbLayerInit <L extends UtilsLang,
+public class DbViewInit <L extends UtilsLang,
  							D extends UtilsDescription,
- 							LAYER extends GeoJsfLayer<L,D>>
+ 							LAYER extends GeoJsfLayer<L,D>,
+ 							VIEW extends GeoJsfView<L,D,LAYER>>
 {
-	final static Logger logger = LoggerFactory.getLogger(DbLayerInit.class);
+	final static Logger logger = LoggerFactory.getLogger(DbViewInit.class);
 	
     private final Class<LAYER> cLayer;
+    private final Class<VIEW> cView;
     
     private UtilsSecurityFacade fSecurity;
     private EjbLangFactory<L> ejbLangFactory;
     private EjbDescriptionFactory<D> ejbDescriptionFactory;
     
-    public DbLayerInit(final Class<L> cL, final Class<D> cD,final Class<LAYER> cLayer, UtilsSecurityFacade fAcl)
+    public DbViewInit(final Class<L> cL, final Class<D> cD,final Class<LAYER> cLayer, final Class<VIEW> cView,UtilsSecurityFacade fAcl)
 	{       
         this.cLayer = cLayer;
+        this.cView = cView;
         
         this.fSecurity=fAcl;
 		
@@ -42,31 +47,29 @@ public class DbLayerInit <L extends UtilsLang,
 	
 	public static <L extends UtilsLang,
 	   			   D extends UtilsDescription, 
-	   			   LAYER extends GeoJsfLayer<L,D>>
-		DbLayerInit<L,D,LAYER>
-		factory(final Class<L> cL,final Class<D> cD,final Class<LAYER> cLayer, UtilsSecurityFacade fAcl)
+	   			   LAYER extends GeoJsfLayer<L,D>,
+	   			   VIEW extends GeoJsfView<L,D,LAYER>>
+		DbViewInit<L,D,LAYER,VIEW>
+		factory(final Class<L> cL,final Class<D> cD,final Class<LAYER> cLayer, final Class<VIEW> cView,UtilsSecurityFacade fAcl)
 	{
-		return new DbLayerInit<L,D,LAYER>(cL,cD,cLayer,fAcl);
+		return new DbViewInit<L,D,LAYER,VIEW>(cL,cD,cLayer,cView,fAcl);
 	}
 	
-	
-	
-	public void iuLayer(Layers layers) throws UtilsConfigurationException
+	public void iuLayer(Views views) throws UtilsConfigurationException
 	{
-		logger.debug("i/u "+Layers.class.getSimpleName()+" with "+layers.getLayer().size()+" "+Layer.class.getSimpleName());
+		logger.debug("i/u "+Views.class.getSimpleName()+" with "+views.getView()+" "+View.class.getSimpleName());
 		
-		AhtDbEjbUpdater<LAYER> updateLayer = AhtDbEjbUpdater.createFactory(cLayer);
-		
-		updateLayer.dbEjbs(fSecurity.all(cLayer));
+		AhtDbEjbUpdater<VIEW> ejbUpdater = AhtDbEjbUpdater.createFactory(cView);
+		ejbUpdater.dbEjbs(fSecurity.all(cView));
 
-		for(Layer layer : layers.getLayer())
+		for(View view : views.getView())
 		{
-			updateLayer.actualAdd(layer.getCode());
+			ejbUpdater.actualAdd(view.getCode());
 			
-			LAYER ejb;
+			VIEW ejb;
 			try
 			{
-				ejb = fSecurity.fByCode(cLayer,layer.getCode());
+				ejb = fSecurity.fByCode(cView,view.getCode());
 				ejbLangFactory.rmLang(fSecurity,ejb);
 				ejbDescriptionFactory.rmDescription(fSecurity,ejb);
 			}
@@ -74,9 +77,9 @@ public class DbLayerInit <L extends UtilsLang,
 			{
 				try
 				{
-					ejb = cLayer.newInstance();
-					ejb.setCode(layer.getCode());
-					ejb = (LAYER)fSecurity.persist(ejb);
+					ejb = cView.newInstance();
+					ejb.setCode(view.getCode());
+					ejb = (VIEW)fSecurity.persist(ejb);
 				}
 				catch (InstantiationException e2) {throw new UtilsConfigurationException(e2.getMessage());}
 				catch (IllegalAccessException e2) {throw new UtilsConfigurationException(e2.getMessage());}
@@ -85,19 +88,20 @@ public class DbLayerInit <L extends UtilsLang,
 			
 			try
 			{
-				ejb.setName(ejbLangFactory.getLangMap(layer.getLangs()));
+//				ejb.setName(ejbLangFactory.getLangMap(view.getLangs()));
 //				aclCategory.setDescription(ejbDescriptionFactory.create(category.getDescriptions()));
-				ejb=(LAYER)fSecurity.update(ejb);
+				ejb=(VIEW)fSecurity.update(ejb);
 				
 			}
 			catch (UtilsContraintViolationException e) {logger.error("",e);}
-			catch (InstantiationException e) {logger.error("",e);}
-			catch (IllegalAccessException e) {logger.error("",e);}
-			catch (UtilsIntegrityException e) {logger.error("",e);}
+//			catch (InstantiationException e) {logger.error("",e);}
+//			catch (IllegalAccessException e) {logger.error("",e);}
+//			catch (UtilsIntegrityException e) {logger.error("",e);}
 			catch (UtilsLockingException e) {logger.error("",e);}
 		}
 		
-		updateLayer.remove(fSecurity);
+		ejbUpdater.remove(fSecurity);
 		logger.trace("initUpdateUsecaseCategories finished");
+
 	}
 }
