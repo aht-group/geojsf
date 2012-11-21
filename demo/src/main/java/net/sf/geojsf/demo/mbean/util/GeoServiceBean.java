2edd06719426863a45e9611305ce49fc0e13c164
thorsten-k <t.kisner@web.de>
2012-11-21 10:49:57 +0000

    GeoServiceBean provides layer definitions (GEO-25)

---
+++ demo/src/main/java/net/sf/geojsf/demo/mbean/util/GeoServiceBean.java
@@ -0,0 +1,89 @@
+package net.sf.geojsf.demo.mbean.util;
+
+import java.io.Serializable;
+import java.util.List;
+
+import javax.annotation.PostConstruct;
+import javax.enterprise.context.SessionScoped;
+import javax.faces.event.AjaxBehaviorEvent;
+import javax.inject.Named;
+
+import net.sf.ahtutils.exception.ejb.UtilsIntegrityException;
+import net.sf.geojsf.model.pojo.openlayers.DefaultGeoJsfLayer;
+import net.sf.geojsf.model.pojo.openlayers.DefaultGeoJsfService;
+import net.sf.geojsf.model.pojo.openlayers.DefaultGeoJsfView;
+import net.sf.geojsf.model.pojo.util.DefaultGeoJsfDescription;
+import net.sf.geojsf.model.pojo.util.DefaultGeoJsfLang;
+import net.sf.geojsf.util.factory.ejb.openlayer.EjbGeoLayerFactory;
+import net.sf.geojsf.util.factory.ejb.openlayer.EjbGeoServiceFactory;
+import net.sf.geojsf.util.factory.ejb.openlayer.EjbGeoViewFactory;
+import net.sf.geojsf.util.factory.jsf.JsfLayerFactory;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@Named @SessionScoped
+public class GeoServiceBean implements Serializable {
+	
+	private static final long serialVersionUID = -4869182190759908416L;
+	final static Logger logger = LoggerFactory.getLogger(GeoServiceBean.class);
+	private String text;
+	
+	private JsfLayerFactory<DefaultGeoJsfLang,DefaultGeoJsfDescription,DefaultGeoJsfLayer,DefaultGeoJsfView,DefaultGeoJsfService> fJsf;
+	private EjbGeoServiceFactory<DefaultGeoJsfLang,DefaultGeoJsfDescription,DefaultGeoJsfLayer,DefaultGeoJsfView,DefaultGeoJsfService> fService;
+	private EjbGeoLayerFactory<DefaultGeoJsfLang,DefaultGeoJsfDescription,DefaultGeoJsfLayer,DefaultGeoJsfView,DefaultGeoJsfService> fLayer;
+	private EjbGeoViewFactory<DefaultGeoJsfLang,DefaultGeoJsfDescription,DefaultGeoJsfLayer,DefaultGeoJsfView,DefaultGeoJsfService> fView;
+	
+	private DefaultGeoJsfService serviceOsm,serviceAht;
+	private DefaultGeoJsfLayer layerOsmBasic,layerAhtRoads,layerAhtStreams;
+	private DefaultGeoJsfView viewMultiService;
+	
+	private List<DefaultGeoJsfService> openLayerMulti;
+
+	@PostConstruct
+	public void init() throws UtilsIntegrityException
+	{
+		logger.info("@PostConstruct");
+		fJsf = JsfLayerFactory.factory();
+		fService = EjbGeoServiceFactory.factory(DefaultGeoJsfService.class);
+		fLayer = EjbGeoLayerFactory.factory(DefaultGeoJsfLayer.class);
+		fView = EjbGeoViewFactory.factory(DefaultGeoJsfView.class);
+		
+		initServices();
+		initLayer();
+		initViews();
+		
+		openLayerMulti = fJsf.build(viewMultiService);
+	}
+	
+	private void initServices() throws UtilsIntegrityException
+	{
+		serviceOsm = fService.create("osm","http://vmap0.tiles.osgeo.org/wms/vmap0");
+		serviceAht = fService.create("aht","https://www.aht-group.com/geoserver/sf/wms");
+	}
+	
+	private void initLayer() throws UtilsIntegrityException
+	{
+		layerOsmBasic = fLayer.create("basic", serviceOsm);
+		layerAhtRoads = fLayer.create("roads",serviceAht);
+		layerAhtStreams = fLayer.create("streams",serviceAht);
+	}
+	
+	private void initViews() throws UtilsIntegrityException
+	{
+		viewMultiService = fView.create("view");
+		viewMultiService.getLayer().add(layerOsmBasic);
+		viewMultiService.getLayer().add(layerAhtRoads);
+		viewMultiService.getLayer().add(layerAhtStreams);
+	}
+	
+	public void listen(AjaxBehaviorEvent evt)
+	{
+		logger.info("Received event: " +evt.toString());
+	}
+
+	public String getText() {return text;}
+	public void setText(String text) {this.text = text;}
+
+	public List<DefaultGeoJsfService> getOpenLayerMulti() {return openLayerMulti;}
+}
