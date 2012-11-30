6ad7eefa63d27ceb119e889e9e55902e70ea89ad
hhemmer <hhemmer@web.de>
2012-11-30 11:51:34 +0000

    Fundmental refactoring of the component (added backing bean, refactored JavaScript API and optimized code of map.xhtml) and added support for adding layers following the new data model (GEO-25) Added some more tests for click handling (GEO-23)

---
+++ component/src/main/resources/META-INF/resources/geojsf/GeoJSF.js
@@ -0,0 +1,120 @@
+// Define the variable where the map Div container will be injected
+var map;
+
+// Define the variable where the panZoomBar OpenLayers control will be injected
+var panZoomBar;
+
+// Define a Namespace for the GeoJSF specific JavaScript functionality
+var GeoJSF = {
+		
+		initMap: function(mapDiv,msOptions)
+		{
+			map = new OpenLayers.Map(mapDiv,msOptions);
+			map.addControl(new OpenLayers.Control.Navigation({'zoomWheelEnabled': false}));
+		    var click = new OpenLayers.Control.Click();
+		    map.addControl(click);
+		    click.activate();
+		    panZoomBar = new OpenLayers.Control.PanZoomBar();
+		    map.addControl(panZoomBar);
+		    for (var p = 0; p < 4; p++) {
+		        panZoomBar.buttons[p].style.display = 'none';
+		    }
+		    panZoomBar.div.style.marginTop = "-50px";
+		    var scalebar = new OpenLayers.Control.ScaleBar({align: "right"});
+		    map.addControl(scalebar);
+		    map.addControl(new OpenLayers.Control.LayerSwitcher());
+		},
+
+		addLoadEvent: function(func)
+		{
+			OpenLayers.Event.observe(window, "load", func ); 
+		},
+		
+		addClickHandler: function (id, inputDOMelement, mapDOMelement)
+		{
+			 OpenLayers.Control.Click = OpenLayers.Class(OpenLayers.Control, {                
+		         defaultHandlerOptions: {
+		             'single': true,
+		             'double': false,
+		             'pixelTolerance': 0,
+		             'stopSingle': false,
+		             'stopDouble': false
+		         },
+
+		         initialize: function(options) {
+		             this.handlerOptions = OpenLayers.Util.extend(
+		                 {}, this.defaultHandlerOptions
+		             );
+		             OpenLayers.Control.prototype.initialize.apply(
+		                 this, arguments
+		             ); 
+		             this.handler = new OpenLayers.Handler.Click(
+		                 this, {
+		                     'click': this.trigger
+		                 }, this.handlerOptions
+		             );
+		         }, 
+
+		         trigger: function(e) {
+		        	 inputDOMelement.val(map.getLonLatFromViewPortPx(e.xy));
+		        	 alert(inputDOMelement.val());
+		         	 
+		        	 var evObj = document.createEvent('MouseEvents');
+		        	 evObj.lonlat = map.getLonLatFromViewPortPx(e.xy);
+		        	 evObj.initEvent('click', true, true);
+		        //	 mapDOMelement.dispatchEvent(evObj);
+		        //	 var e = jQuery.Event("click", { lonlat: evObj.lonlat });
+		        //	 jQuery(id).trigger(e);
+		        	 jsf.ajax.request(id, "click",{execute:"@form",render: id,params: evObj.lonlat});
+		         }
+		     });
+		},
+		
+		// This function adds a OpenStreetMap base layer
+		initLayer: function() 
+		{
+			var baseLayer = new OpenLayers.Layer.WMS(
+					  'OSM WMS', 
+					  'http://vmap0.tiles.osgeo.org/wms/vmap0', 
+					  {
+					    layers: 'basic',
+					    transparent: "true",
+					    format: 'image/png'
+					  }, 
+					  {
+						  isBaseLayer: true
+					    }
+					 );
+			
+			map.addLayer(baseLayer);
+			map.zoomToMaxExtent();
+			for (var p = 0; p < 4; p++)
+			{
+				panZoomBar.buttons[p].style.display = 'none';
+			}
+		},
+		
+		// Remove all layers and add the base layer again
+		resetLayers: function()
+		{
+			for (var i = map.layers.length - 1; i >= 0; i--) {
+			    map.removeLayer(map.layers[i]);
+			}
+			GeoJSF.initLayer();
+		},
+		
+		addLayer: function(name, url, params)
+		{
+			var layer = new OpenLayers.Layer.WMS(
+					  name, 
+					  url, 
+					  params);
+			
+			map.addLayer(layer);
+			map.zoomToMaxExtent();
+			for (var p = 0; p < 4; p++)
+			{
+				panZoomBar.buttons[p].style.display = 'none';
+			}
+		}
+}
