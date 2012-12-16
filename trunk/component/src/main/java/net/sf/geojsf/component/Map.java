1da232b7fde8791df9f59a50716e0895f82bb42f
thorsten-k <thorsten-k@d7d7bd7b-d12e-478d-8a14-7487b8a8350c>
2012-12-16 12:19:45 +0000

    Factories now create name-map

--- trunk/component/src/main/java/net/sf/geojsf/component/Map.java
+++ trunk/component/src/main/java/net/sf/geojsf/component/Map.java
@@ -8,8 +8,12 @@ import javax.faces.component.UINamingContainer;
 import javax.faces.context.FacesContext;
 import javax.faces.event.AjaxBehaviorEvent;
 
-import net.sf.geojsf.model.pojo.openlayers.DefaultGeoJsfLayer;
-import net.sf.geojsf.model.pojo.openlayers.DefaultGeoJsfService;
+import net.sf.ahtutils.model.interfaces.status.UtilsDescription;
+import net.sf.ahtutils.model.interfaces.status.UtilsLang;
+import net.sf.geojsf.model.interfaces.openlayers.GeoJsfLayer;
+import net.sf.geojsf.model.interfaces.openlayers.GeoJsfService;
+import net.sf.geojsf.model.interfaces.openlayers.GeoJsfView;
+import net.sf.geojsf.model.interfaces.openlayers.GeoJsfViewLayer;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -19,11 +23,12 @@ public class Map extends UINamingContainer{
 	
 	final static Logger logger = LoggerFactory.getLogger(Map.class);
 	
-	public String layerString(DefaultGeoJsfService service)
+	public <L extends UtilsLang,D extends UtilsDescription,SERVICE extends GeoJsfService<L,D,SERVICE,LAYER,VIEW,VL>,LAYER extends GeoJsfLayer<L,D,SERVICE,LAYER,VIEW,VL>,VIEW extends GeoJsfView<L,D,SERVICE,LAYER,VIEW,VL>,VL extends GeoJsfViewLayer<L,D,SERVICE,LAYER,VIEW,VL>> 
+		String layerString(SERVICE service)
 	{
-		ArrayList<DefaultGeoJsfLayer> layers = (ArrayList<DefaultGeoJsfLayer>) service.getLayer();
+		ArrayList<LAYER> layers = (ArrayList<LAYER>) service.getLayer();
 		StringBuffer sb = new StringBuffer();
-		for (DefaultGeoJsfLayer layer : layers)
+		for (LAYER layer : layers)
 		{
 			sb.append(layer.getCode() +",");
 		}
--- trunk/util/src/main/java/net/sf/geojsf/util/factory/ejb/openlayer/EjbGeoViewFactory.java
+++ trunk/util/src/main/java/net/sf/geojsf/util/factory/ejb/openlayer/EjbGeoViewFactory.java
@@ -1,5 +1,6 @@
 package net.sf.geojsf.util.factory.ejb.openlayer;
 
+import net.sf.ahtutils.controller.factory.ejb.status.EjbLangFactory;
 import net.sf.ahtutils.exception.ejb.UtilsIntegrityException;
 import net.sf.ahtutils.model.interfaces.status.UtilsDescription;
 import net.sf.ahtutils.model.interfaces.status.UtilsLang;
@@ -15,23 +16,29 @@ public class EjbGeoViewFactory<L extends UtilsLang,D extends UtilsDescription,SE
 {
 	final static Logger logger = LoggerFactory.getLogger(EjbGeoViewFactory.class);
 	
-	final Class<VIEW> clView;
-	
-    public EjbGeoViewFactory(final Class<VIEW> clView)
-    {
-        this.clView = clView;
-    } 
+	final Class<VIEW> cView;
+	private EjbLangFactory<L> fLang;
     
     public static <L extends UtilsLang,D extends UtilsDescription,SERVICE extends GeoJsfService<L,D,SERVICE,LAYER,VIEW,VL>, LAYER extends GeoJsfLayer<L,D,SERVICE,LAYER,VIEW,VL>,VIEW extends GeoJsfView<L,D,SERVICE,LAYER,VIEW,VL>, VL extends GeoJsfViewLayer<L,D,SERVICE,LAYER,VIEW,VL>>
-    	EjbGeoViewFactory<L,D,SERVICE,LAYER,VIEW,VL> factory(final Class<VIEW> clView)
+    	EjbGeoViewFactory<L,D,SERVICE,LAYER,VIEW,VL> factory(final Class<L> cLang,final Class<VIEW> cView)
     {
-        return new EjbGeoViewFactory<L,D,SERVICE,LAYER,VIEW,VL>(clView);
+        return new EjbGeoViewFactory<L,D,SERVICE,LAYER,VIEW,VL>(cLang,cView);
     }
+    
+    public EjbGeoViewFactory(final Class<L> cLang,final Class<VIEW> cView)
+    {
+        this.cView = cView;
+        fLang = EjbLangFactory.createFactory(cLang);
+    } 
 	
-	public VIEW create(String code, int zoom, double x, double y) throws UtilsIntegrityException
+	public VIEW create(String code, int zoom, double x, double y, String[] langKeys) throws UtilsIntegrityException
 	{
 		VIEW ejb;
-		try {ejb = clView.newInstance();}
+		try
+		{
+			ejb = cView.newInstance();
+			ejb.setName(fLang.createEmpty(langKeys));
+		}
 		catch (InstantiationException e) {throw new UtilsIntegrityException(e.getMessage());}
 		catch (IllegalAccessException e) {throw new UtilsIntegrityException(e.getMessage());}
 		ejb.setCode(code);
