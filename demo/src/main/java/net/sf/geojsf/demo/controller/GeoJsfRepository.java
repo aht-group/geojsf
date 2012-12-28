f307162967b4f72b6060d426e98ddde09e772227
thorsten-k <t.kisner@web.de>
2012-12-28 09:05:12 +0000

    ahtutils-security added, client module added (GEO-44)

---
+++ demo/src/main/java/net/sf/geojsf/demo/controller/GeoJsfRepository.java
@@ -0,0 +1,16 @@
+package net.sf.geojsf.demo.controller;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+import javax.inject.Qualifier;
+
+@Qualifier
+@Target({ ElementType.TYPE, ElementType.METHOD, ElementType.PARAMETER, ElementType.FIELD })
+@Retention(RetentionPolicy.RUNTIME)
+public @interface GeoJsfRepository
+{
+    /* class body intentionally left blank */
+}
