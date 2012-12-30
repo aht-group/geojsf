783ef2d480c392af1f96bde4d302d77efae2553b
thorsten-k <t.kisner@web.de>
2012-12-30 08:06:11 +0000

    GeoServer Factories added (GEO-46)

---
+++ interfaces/src/main/java/org/geojsf/controller/interfaces/GeoServerConfig.java
@@ -0,0 +1,13 @@
+package org.geojsf.controller.interfaces;
+
+public interface GeoServerConfig 
+{		
+	public static String workspace = "geoserver.workspace";
+	
+	public static String restHost = "geoserver.rest.host";
+	public static String restUser = "geoserver.rest.user";
+	public static String restPassword = "geoserver.rest.password";
+	
+	public static String dsName = "geoserver.ds.name";
+	public static String dsDescription = "geoserver.ds.description";
+}
