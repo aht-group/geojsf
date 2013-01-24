f10fcd8145cd5916febb574700dd7789c4c6d786
thorsten-k <thorsten-k@d7d7bd7b-d12e-478d-8a14-7487b8a8350c>
2013-01-24 08:40:27 +0000

    PostGIS  tutorial updated

--- trunk/doc/src/main/resources/code.geojsf/tutorial/postgis/db.sql
+++ trunk/doc/src/main/resources/code.geojsf/tutorial/postgis/db.sql
@@ -1,11 +1,11 @@
 CREATE USER geo WITH PASSWORD 'geo';  
 
-SELECT pg_terminate_backend(procpid) FROM pg_stat_activity WHERE procpid <> pg_backend_pid() AND datname = 'geo';
+# This is helpful if your want to drop a existing databse
+SELECT pg_terminate_backend(procpid) FROM pg_stat_activity
+       WHERE procpid <> pg_backend_pid() AND datname = 'geo';
 DROP DATABASE geo;
-CREATE DATABASE geo OWNER lis ENCODING 'UTF8';
+
+# Create the database
+CREATE DATABASE geo OWNER geo ENCODING 'UTF8';
 GRANT ALL PRIVILEGES ON DATABASE geo TO geo;
 createlang plpgsql geo;
-
-psql -U postgres -d geo -f /usr/share/postgresql/9.1/contrib/postgis-2.0/postgis.sql
-psql -U postgres -d geo -f /usr/share/postgresql/9.1/contrib/postgis-2.0/postgis_comments.sql
-psql -U postgres -d geo -f /usr/share/postgresql/9.1/contrib/postgis-2.0/spatial_ref_sys.sql
--- trunk/doc/src/main/resources/code.geojsf/tutorial/postgis/pre.sh
+++ trunk/doc/src/main/resources/code.geojsf/tutorial/postgis/pre.sh
@@ -1,5 +1,3 @@
 apt-get -t squeeze-backports install postgresql-9.1 postgresql-server-dev-9.1
-
 apt-get install libgdal1-dev
-
 apt-get install build-essential libproj-dev libxml2-dev 
