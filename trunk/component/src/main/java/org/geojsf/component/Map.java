f7aa834fcf814ac066ee6e9c6d9b7c024635a238
hhemmer <hhemmer@d7d7bd7b-d12e-478d-8a14-7487b8a8350c>
2013-03-27 09:48:55 +0000

    Implemented AJAX-triggered listener for passing the coordinates of the map-click to the backing bean (GEO-23)

--- trunk/component/src/main/java/org/geojsf/component/Map.java
+++ trunk/component/src/main/java/org/geojsf/component/Map.java
@@ -1,12 +1,16 @@
 package org.geojsf.component;
 
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Hashtable;
 import java.util.List;
 
+import javax.el.MethodExpression;
 import javax.faces.component.FacesComponent;
 import javax.faces.component.UINamingContainer;
+import javax.faces.component.behavior.ClientBehavior;
+import javax.faces.component.behavior.ClientBehaviorHolder;
 import javax.faces.context.FacesContext;
-import javax.faces.event.AjaxBehaviorEvent;
 
 import net.sf.ahtutils.model.interfaces.status.UtilsDescription;
 import net.sf.ahtutils.model.interfaces.status.UtilsLang;
@@ -18,14 +22,17 @@ import org.geojsf.model.interfaces.openlayers.GeoJsfView;
 import org.geojsf.model.interfaces.openlayers.GeoJsfViewLayer;
 import org.geojsf.model.pojo.openlayers.DefaultGeoJsfLayer;
 import org.geojsf.model.pojo.openlayers.DefaultGeoJsfService;
+import org.geojsf.xml.gml.Coordinates;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 @FacesComponent(value="org.geojsf.component.Map")
-public class Map extends UINamingContainer{
+public class Map  extends UINamingContainer implements ClientBehaviorHolder{
 	
 	final static Logger logger = LoggerFactory.getLogger(Map.class);
 	private ArrayList<DefaultGeoJsfService> serviceList;
+	
+	private Coordinates coords = new Coordinates();
 	
 	public <L extends UtilsLang,D extends UtilsDescription,SERVICE extends GeoJsfService<L,D,SERVICE,LAYER,VIEW,VL>,LAYER extends GeoJsfLayer<L,D,SERVICE,LAYER,VIEW,VL>,VIEW extends GeoJsfView<L,D,SERVICE,LAYER,VIEW,VL>,VL extends GeoJsfViewLayer<L,D,SERVICE,LAYER,VIEW,VL>> 
 		String layerString(SERVICE service)
@@ -39,29 +46,23 @@ public class Map extends UINamingContainer{
 		return sb.deleteCharAt(sb.length()-1).toString();
 	}
 	
-	public void listen(AjaxBehaviorEvent evt)
-	{
-		logger.error("Received event: " +evt.toString());
-	}
-	
+	@SuppressWarnings({ "rawtypes", "unchecked" })
 	public String fallback()
 	{
-		
 		 serviceList = new ArrayList<DefaultGeoJsfService>();
-		 logger.info("Checking value existence ...");
+		 logger.debug("Checking value existence ...");
 		 if (getAttributes().get("value")==null)
 		 {
-			 logger.info("No value given - falling back to simple version");
+			 logger.debug("No value given - falling back to simple version");
 			 String url = (String) getAttributes().get("simpleWmsUrl");
 			 DefaultGeoJsfService service = new DefaultGeoJsfService();
 			 service.setUrl(url);
 			 service.setCode("BaseLayer");
-			 ArrayList<String> layers = new ArrayList<String>();
 			 Object layerList = getAttributes().get("simpleLayers");
-			 logger.info("Detecting layer definition type..." +layerList.getClass().getSimpleName());
+			 logger.debug("Detecting layer definition type..." +layerList.getClass().getSimpleName());
 			 if (layerList.getClass().getSimpleName().equals("String"))
 			 {
-				 logger.info("Detected layer list given as Strings");
+				 logger.debug("Detected layer list given as Strings");
 				 String layerString = (String)layerList;
 				 for (String string : layerString.split(","))
 				 {
@@ -83,21 +84,24 @@ public class Map extends UINamingContainer{
 		return new String();
 	}
 	
-	public void decode(FacesContext context)
-	{
-		logger.debug("Map Component is now entering DECODE stage");
-	}
-	
 	public void listenRedirect(javax.faces.event.ValueChangeEvent evt)
     {
-		logger.info("Value of coordinates changed by click event. Changed from: " +evt.getOldValue() +" to " +evt.getNewValue());
-        FacesContext context = FacesContext.getCurrentInstance();
-    //    Map map = (Map)getComponent();
-     //   context.getViewRoot().addClientBehavior(eventName, behavior)
-    //    MethodExpression ajaxEventListener = (MethodExpression) getAttributes().get("listener");
-    //    ajaxEventListener.invoke(context.getELContext(), new Object[] {});
+		logger.debug("Value of coordinates changed by click event. Changed from: " +evt.getOldValue() +" to " +evt.getNewValue());
+		coords = new Coordinates(); 
+		coords.setValue(evt.getNewValue().toString());
+		try {
+		//Invoke the given listener that takes the coordinates
+		FacesContext context = FacesContext.getCurrentInstance();
+        MethodExpression coordinatesListener = (MethodExpression) getAttributes().get("listener");
+        Object[] arguments = new Object[1];
+        arguments[0] = coords;
+        coordinatesListener.invoke(context.getELContext(), arguments);
+		}
+		catch(Exception ex){
+			logger.error("no coordinates listener set - please set the listener attribute to point to a method that accepts org.geojsf.xml.gml.Coordinates");
+		}
     }
-
+	
 	public ArrayList<DefaultGeoJsfService> getServiceList() {
 		return serviceList;
 	}
@@ -106,4 +110,34 @@ public class Map extends UINamingContainer{
 		this.serviceList = serviceList;
 	}
 
+	@Override
+	public void addClientBehavior(String eventName, ClientBehavior behavior) {
+		logger.debug("Adding client behavior for " +eventName);
+		logger.debug("Publishing new MapClickEvent...");
+	}
+
+	@Override
+	public Collection<String> getEventNames() {
+		ArrayList<String> events = new ArrayList<String>();
+		events.add("mapClick");
+		return events;
+	}
+
+	@Override
+	public java.util.Map<String, List<ClientBehavior>> getClientBehaviors() {
+		return new Hashtable<String, List<ClientBehavior>>();
+	}
+
+	@Override
+	public String getDefaultEventName() {
+		return "mapClick";
+	}
+
+	public Coordinates getCoords() {
+		return coords;
+	}
+
+	public void setCoords(Coordinates coords) {
+		this.coords = coords;
+	}
 }
