befe4156e5c6412363bfe7e4b587bbfd91499e1a
thorsten-k <thorsten-k@d7d7bd7b-d12e-478d-8a14-7487b8a8350c>
2013-06-16 13:27:16 +0000

    Individual JS script for coordinate (GEO-68)

--- trunk/component/src/main/java/org/geojsf/component/Coordinate.java
+++ trunk/component/src/main/java/org/geojsf/component/Coordinate.java
@@ -2,6 +2,8 @@ package org.geojsf.component;
 
 import java.io.IOException;
 
+import javax.faces.application.ResourceDependencies;
+import javax.faces.application.ResourceDependency;
 import javax.faces.component.FacesComponent;
 import javax.faces.component.UINamingContainer;
 import javax.faces.component.behavior.ClientBehaviorHolder;
@@ -11,6 +13,9 @@ import javax.faces.context.ResponseWriter;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+@ResourceDependencies({
+	@ResourceDependency(library="js.geojsf", name="coordinate.js", target="head")
+})
 @FacesComponent(value="org.geojsf.component.Coordinate")
 public class Coordinate extends UINamingContainer implements ClientBehaviorHolder
 {
@@ -25,7 +30,7 @@ public class Coordinate extends UINamingContainer implements ClientBehaviorHolde
 		ResponseWriter writer = ctx.getResponseWriter();
 		
 		writer.startElement("script", this);
-			writer.writeText("GeoJSF.addCoordinatesControl('" +getOrientation() +"');", null);
+			writer.writeText("GeoJsfCoordinate.addCoordinatesControl('" +getOrientation() +"');", null);
  		writer.endElement("script");   
 	}
 
