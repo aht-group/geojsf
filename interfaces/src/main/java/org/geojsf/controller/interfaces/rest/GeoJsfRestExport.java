5d862ba9e27ea1c775844787a801ed062496961c
thorsten-k <t.kisner@web.de>
2013-06-26 06:14:39 +0000

    Preparing REST GeoJsf import/export (GEO-75)

---
+++ interfaces/src/main/java/org/geojsf/controller/interfaces/rest/GeoJsfRestExport.java
@@ -0,0 +1,25 @@
+package org.geojsf.controller.interfaces.rest;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+import org.geojsf.xml.openlayers.Layers;
+import org.geojsf.xml.openlayers.Repository;
+import org.geojsf.xml.openlayers.Views;
+
+public interface GeoJsfRestExport
+{	
+	@GET @Path("/services")
+	@Produces(MediaType.APPLICATION_XML)
+	Repository exportServices();
+	
+	@GET @Path("/layers")
+	@Produces(MediaType.APPLICATION_XML)
+	Layers exportLayers();
+	
+	@GET @Path("/views")
+	@Produces(MediaType.APPLICATION_XML)
+	Views exportViews();
+}
