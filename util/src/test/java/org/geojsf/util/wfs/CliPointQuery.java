6dd5f088cf4377c4fe19ccb00f8856238762f9e7
thorsten-k <t.kisner@web.de>
2013-07-11 05:36:10 +0000

    Plain WFS Request successful (GEO-84)

--- util/src/test/java/org/geojsf/util/wfs/CliPointQuery.java
+++ util/src/test/java/org/geojsf/util/wfs/CliPointQuery.java
@@ -3,12 +3,14 @@ package org.geojsf.util.wfs;
 import java.util.ArrayList;
 import java.util.List;
 
-import net.sf.exlp.util.xml.JaxbUtil;
+import net.sf.exlp.util.xml.JDomUtil;
 
 import org.apache.commons.configuration.Configuration;
 import org.geojsf.test.AbstractGeoJsfUtilTest;
 import org.geojsf.test.GeoJsfUtilsTestBootstrap;
 import org.geojsf.util.GeoServerConfigKeys;
+import org.geojsf.xml.gml.Coordinates;
+import org.geojsf.xml.ogc.Distance;
 import org.geojsf.xml.wfs.GetFeature;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -27,16 +29,20 @@ public class CliPointQuery extends AbstractGeoJsfUtilTest
 	
 	public void pointQuery()
 	{
-		OpenLayers ol = new OpenLayers("Dam_sites");
-		ol.getSearchRadius().setPixel(8);
-		
 		List<String> properties = new ArrayList<String>();
-		properties.add("dam_name");
-		String layerCode = "dam_sites";
-		GetFeature gf = PointQueryFactory.cGetFeature("LCBC:"+layerCode,properties,ol.getClick().toGmlCoordinates(),ol.createSearchDistance());	
-		JaxbUtil.info(gf);
-		WfsHttpRequest r = new WfsHttpRequest(config.getString(GeoServerConfigKeys.restHost));
-		List<Integer> lIds = r.ejbIdRequest(gf,"LCBC",layerCode);
+		properties.add("city_name");
+		
+		Coordinates coordinates = new Coordinates();
+		coordinates.setValue("18.337941894531248,13.19704345703125");
+		
+		Distance distance = new Distance();
+		distance.setUnits("degree");
+		distance.setValue("1.074");
+		
+		GetFeature gf = PointQueryFactory.cGetFeature(config.getString("geoserver.test.pointquery.layer"),properties,coordinates,distance);
+		
+		WfsHttpRequest r = new WfsHttpRequest(config.getString(GeoServerConfigKeys.restHost)+"/wcs");
+		JDomUtil.debug(r.request(gf));
 	}
 	
 	public void wfsRequest()
@@ -49,6 +55,6 @@ public class CliPointQuery extends AbstractGeoJsfUtilTest
 		Configuration config = GeoJsfUtilsTestBootstrap.init();
 		CliPointQuery cli = new CliPointQuery(config);
 		cli.pointQuery();
-		cli.wfsRequest();
+//		cli.wfsRequest();
 	}
 }
