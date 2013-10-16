9ff42839e3b09da548ace31bf905d00820bd1ab5
thorsten-k <thorsten-k@d7d7bd7b-d12e-478d-8a14-7487b8a8350c>
2013-10-16 15:23:58 +0000

    MultiPolygon and WKT Factory added

---
+++ trunk/util/src/main/java/org/geojsf/factory/xml/geojsf/XmlWktFactory.java
@@ -0,0 +1,31 @@
+package org.geojsf.factory.xml.geojsf;
+
+import java.io.Serializable;
+
+import org.geojsf.xml.geojsf.Wkt;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.vividsolutions.jts.geom.Geometry;
+import com.vividsolutions.jts.io.WKTWriter;
+
+public class XmlWktFactory implements Serializable
+{
+	final static Logger logger = LoggerFactory.getLogger(XmlWktFactory.class);
+	
+	public static final long serialVersionUID=1;
+	
+	// >>>>>>>>>>>>>>>>>>>>>>>>>>Fields<<<<<<<<<<<<<<<<<<<<<<<<<<<	
+
+	public static Wkt build(Geometry geometry)
+	{
+		WKTWriter w = new WKTWriter();
+		
+		Wkt wkt = new Wkt();
+		
+		wkt.setType(geometry.getClass().getSimpleName());
+		wkt.setValue(w.write(geometry));
+
+		return wkt;
+	}
+}
