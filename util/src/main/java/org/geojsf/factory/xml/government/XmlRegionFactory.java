0646e48c9d4d56b587808d2bc9435d98e4476328
thorsten-k <t.kisner@web.de>
2013-11-08 11:14:24 +0000

    XmlRegionFactory added

---
+++ util/src/main/java/org/geojsf/factory/xml/government/XmlRegionFactory.java
@@ -0,0 +1,20 @@
+package org.geojsf.factory.xml.government;
+
+import java.io.Serializable;
+
+import org.geojsf.xml.government.Region;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class XmlRegionFactory implements Serializable
+{
+	final static Logger logger = LoggerFactory.getLogger(XmlRegionFactory.class);
+	public static final long serialVersionUID=1;
+	
+	public static Region create(String code)
+	{
+        Region xml = new Region();
+		xml.setCode(code);
+		return xml;
+	}
+}
