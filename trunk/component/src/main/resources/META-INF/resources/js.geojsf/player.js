99d75dd6b7e7f39c7c7e04619ef42d6ef4569c18
hhemmer <hhemmer@d7d7bd7b-d12e-478d-8a14-7487b8a8350c>
2014-01-01 21:39:27 +0000

    Added skeleton for WMS-T player JavaScript control (GEO-80)

---
+++ trunk/component/src/main/resources/META-INF/resources/js.geojsf/player.js
@@ -0,0 +1,33 @@
+var GeoJsfPlayer = {
+		
+		timestep: null,
+		currentTime: null,
+
+		play : function()
+		{
+			//Here needs to happen the following:
+			//
+			GeoJSF.map.setCenter(new OpenLayers.LonLat(lon, lat), zoom);
+		},
+		
+		stop : function()
+		{
+			//Here needs to happen the following:
+			//
+			GeoJSF.map.setCenter(new OpenLayers.LonLat(lon, lat), zoom);
+		},
+		
+		pause : function()
+		{
+			//Here needs to happen the following:
+			//
+			GeoJSF.map.setCenter(new OpenLayers.LonLat(lon, lat), zoom);
+		},
+		
+		reset : function()
+		{
+			//Here needs to happen the following:
+			//
+			GeoJSF.map.setCenter(new OpenLayers.LonLat(lon, lat), zoom);
+		},
+};
