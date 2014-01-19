aac230fc8b61629f412872d2260cf3f06220e75e
hhemmer <hhemmer@web.de>
2014-01-19 14:07:30 +0000

    Fixed missing dependency for Joda Time.

--- util/src/main/java/org/geojsf/factory/txt/TxtIsoTimeFactory.java
+++ util/src/main/java/org/geojsf/factory/txt/TxtIsoTimeFactory.java
@@ -5,10 +5,6 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.TimeZone;
 
-import org.joda.time.Chronology;
-import org.joda.time.DateTime;
-import org.joda.time.DateTimeZone;
-import org.joda.time.chrono.ISOChronology;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
