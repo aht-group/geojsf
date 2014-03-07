fef348b7f12da7aa6978a46c27c5aa0857e90263
thorsten-k <t.kisner@web.de>
2014-03-07 09:41:35 +0000

    Scale added (GEO-109)

---
+++ util/src/main/java/org/geojsf/factory/txt/TxtScaleFactory.java
@@ -0,0 +1,29 @@
+package org.geojsf.factory.txt;
+
+import java.math.RoundingMode;
+import java.text.DecimalFormat;
+
+import org.geojsf.xml.geojsf.Scale;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class TxtScaleFactory
+{
+	final static Logger logger = LoggerFactory.getLogger(TxtScaleFactory.class);
+	
+	private DecimalFormat df;
+	
+	public TxtScaleFactory()
+	{
+		df = new DecimalFormat("#.0");
+		df.setRoundingMode(RoundingMode.HALF_EVEN);
+	}
+	
+	public String build(Scale scale)
+	{
+		StringBuffer sb = new StringBuffer();
+		sb.append(" scale: ").append(df.format(scale.getValue()));
+		sb.append(" [").append(scale.getUnit()).append("]");
+		return sb.toString();
+	}
+}
