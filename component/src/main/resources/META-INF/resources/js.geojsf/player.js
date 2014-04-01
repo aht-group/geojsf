e891559d5f0e00243fcff2fb7ead916f26a542f1
hhemmer <hhemmer@web.de>
2014-04-01 07:58:49 +0000

    Some changes related to WMS-t animation player (GEO-80)

--- component/src/main/resources/META-INF/resources/js.geojsf/player.js
+++ component/src/main/resources/META-INF/resources/js.geojsf/player.js
@@ -2,32 +2,36 @@ var GeoJsfPlayer = {
 		
 		timestep: null,
 		currentTime: null,
-
-		play : function()
+		playing: false,
+		temporalLayers: null,
+		
+		setTemporalLayers : function(layers)
 		{
-			//Here needs to happen the following:
-			//
-			GeoJSF.map.setCenter(new OpenLayers.LonLat(lon, lat), zoom);
+			var temporalLayers = JSON.parse( layers );
+		},
+		
+		setTime : function(time)
+		{
+			currentTime = time;
+			var isoDate = new Date(time).toISOString();
+			console.log("Time of player is set to " +isoDate +" in ISO format.");
+		},
+
+		start : function()
+		{
+			this.playing = true;
+			while(this.playing==true)
+			{
+				currentTime = currentTime + timeStep;
+				for (var layer in timeLayers)
+					{
+						GeoJSF.updateTime(layer, currentTime);
+					}
+			}
 		},
 		
 		stop : function()
 		{
-			//Here needs to happen the following:
-			//
-			GeoJSF.map.setCenter(new OpenLayers.LonLat(lon, lat), zoom);
-		},
-		
-		pause : function()
-		{
-			//Here needs to happen the following:
-			//
-			GeoJSF.map.setCenter(new OpenLayers.LonLat(lon, lat), zoom);
-		},
-		
-		reset : function()
-		{
-			//Here needs to happen the following:
-			//
-			GeoJSF.map.setCenter(new OpenLayers.LonLat(lon, lat), zoom);
-		},
+			this.playing = false;
+		}
 };
