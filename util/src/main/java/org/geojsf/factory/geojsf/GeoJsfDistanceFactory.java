fcbb9275b386ff32961319566897ee55a2e483bf
thorsten-k <t.kisner@web.de>
2014-05-14 08:01:47 +0000

    GeoJsfDistanceFactory added

---
+++ util/src/main/java/org/geojsf/factory/geojsf/GeoJsfDistanceFactory.java
@@ -0,0 +1,20 @@
+package org.geojsf.factory.geojsf;
+
+import org.geojsf.xml.ogc.Distance;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class GeoJsfDistanceFactory
+{
+	final static Logger logger = LoggerFactory.getLogger(GeoJsfDistanceFactory.class);
+	
+	public static Distance build(int px, float scale)
+	{
+		double radius = scale * px / 3300;
+		
+		Distance distance = new Distance();
+		distance.setUnits("meter");
+		distance.setValue(radius+"");
+		return distance;
+	}
+}
