671bc5c50f3e3f77045f4be594da66fb125765d4
thorsten-k <thorsten-k@d7d7bd7b-d12e-478d-8a14-7487b8a8350c>
2014-05-17 09:34:19 +0000

    Consolidated GeoJsfRest to Improt and Export

--- trunk/interfaces/src/main/java/org/geojsf/interfaces/rest/db/GeoJsfDatabaseExportRest.java
+++ trunk/interfaces/src/main/java/org/geojsf/interfaces/rest/db/GeoJsfDatabaseExportRest.java
@@ -1,8 +1,6 @@
-package org.geojsf.interfaces.rest;
+package org.geojsf.interfaces.rest.db;
 
-import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
-import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
@@ -11,22 +9,22 @@ import org.geojsf.xml.geojsf.Layers;
 import org.geojsf.xml.geojsf.Maps;
 import org.geojsf.xml.geojsf.Repository;
 
-public interface GeoJsfDatabaseRest
+public interface GeoJsfDatabaseExportRest
 {	
-	@GET @Path("/export/services")
+	@GET @Path("/geojsf/services")
 	@Produces(MediaType.APPLICATION_XML)
 	Repository exportServices();
 	
-	@GET @Path("/export/layers")
+	@GET @Path("/geojsf/layers")
 	@Produces(MediaType.APPLICATION_XML)
 	Layers exportLayers();
 	
-	@GET @Path("/export/views")
+	@GET @Path("/geojsf/views")
 	@Produces(MediaType.APPLICATION_XML)
 	Maps exportMaps();
 	
-	@POST @Path("/import")
-	@Produces(MediaType.TEXT_PLAIN)
-	@Consumes(MediaType.APPLICATION_XML)
-	String importGeoJsf(Repository repository);
+//	@POST @Path("/import")
+//	@Produces(MediaType.TEXT_PLAIN)
+//	@Consumes(MediaType.APPLICATION_XML)
+//	String importGeoJsf(Repository repository);
 }
