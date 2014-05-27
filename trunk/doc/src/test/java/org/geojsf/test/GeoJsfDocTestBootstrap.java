332e92557afa8f0e3e78b4477f543297fc67ec11
thorsten-k <thorsten-k@d7d7bd7b-d12e-478d-8a14-7487b8a8350c>
2014-05-27 10:51:59 +0000

    Dummy ER Diagram added

---
+++ trunk/doc/src/test/java/org/geojsf/test/GeoJsfDocTestBootstrap.java
@@ -0,0 +1,20 @@
+package org.geojsf.test;
+
+import net.sf.exlp.util.io.LoggerInit;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class GeoJsfDocTestBootstrap
+{
+	final static Logger logger = LoggerFactory.getLogger(GeoJsfDocTestBootstrap.class);
+		
+	public static void init()
+	{
+		LoggerInit loggerInit = new LoggerInit("log4j.xml");	
+		loggerInit.addAltPath("config.geojsf-doc.test");
+		loggerInit.init();
+		
+//		JaxbUtil.setNsPrefixMapper(new GeoJsfNsPrefixMapper());
+	}
+}
