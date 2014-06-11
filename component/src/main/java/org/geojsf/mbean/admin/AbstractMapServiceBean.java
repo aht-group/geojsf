fc4b8f3515eac408c1810c137627d98a0411d779
thorsten-k <t.kisner@web.de>
2014-06-11 10:29:53 +0000

    generic logic in AbstractMapServiceBean (GEO-130)

--- component/src/main/java/org/geojsf/mbean/admin/AbstractMapServiceBean.java
+++ component/src/main/java/org/geojsf/mbean/admin/AbstractMapServiceBean.java
@@ -13,7 +13,9 @@ import net.sf.ahtutils.model.interfaces.status.UtilsDescription;
 import net.sf.ahtutils.model.interfaces.status.UtilsLang;
 
 import org.geojsf.factory.ejb.EjbGeoLayerFactory;
+import org.geojsf.factory.ejb.EjbGeoMapFactory;
 import org.geojsf.factory.ejb.EjbGeoServiceFactory;
+import org.geojsf.factory.ejb.EjbGeoViewFactory;
 import org.geojsf.interfaces.facade.GeoJsfUtilsFacade;
 import org.geojsf.interfaces.model.GeoJsfLayer;
 import org.geojsf.interfaces.model.GeoJsfMap;
@@ -30,8 +32,10 @@ public class AbstractMapServiceBean <L extends UtilsLang,D extends UtilsDescript
 	
 	protected EjbLangFactory<L> efLang;
 	protected EjbDescriptionFactory<D> efDescription;
-	protected EjbGeoServiceFactory<L,D,SERVICE,LAYER,MAP,VIEW> fService;
-	protected EjbGeoLayerFactory<L,D,SERVICE,LAYER,MAP,VIEW> fLayer;
+	protected EjbGeoServiceFactory<L,D,SERVICE,LAYER,MAP,VIEW> efService;
+	protected EjbGeoLayerFactory<L,D,SERVICE,LAYER,MAP,VIEW> efLayer;
+	protected EjbGeoMapFactory<L,D,SERVICE,LAYER,MAP,VIEW> efMap;
+	protected EjbGeoViewFactory<L,D,SERVICE,LAYER,MAP,VIEW> efView;
 	
 	protected GeoJsfUtilsFacade fGeo;
 	
@@ -40,6 +44,9 @@ public class AbstractMapServiceBean <L extends UtilsLang,D extends UtilsDescript
 	
 	protected List<SERVICE> services;
 	public List<SERVICE> getServices() {return services;}
+	
+	protected List<LAYER> layers;
+	public List<LAYER> getLayers() {return layers;}
 	
 	protected SERVICE service;
 	public SERVICE getService() {return service;}
@@ -50,25 +57,29 @@ public class AbstractMapServiceBean <L extends UtilsLang,D extends UtilsDescript
 	public void setLayer(LAYER layer) {this.layer = layer;}
 	
 	private String[] langKeys;
-	private Class<SERVICE> clService;
+	private Class<SERVICE> cService;
+	private Class<LAYER> cLayer;
 	
-	public void initSuper(String[] langKeys, final Class<L> clLang, final Class<D> clDescription, final Class<SERVICE> clService)
+	public void initSuper(String[] langKeys, final Class<L> cLang, final Class<D> clDescription, final Class<SERVICE> cService, final Class<LAYER> cLayer, final Class<MAP> cMap, final Class<VIEW> cView)
 	{
 		this.langKeys=langKeys;
-		this.clService=clService;
-		efLang = EjbLangFactory.createFactory(clLang);
+		this.cService=cService;
+		this.cLayer=cLayer;
+		efLang = EjbLangFactory.createFactory(cLang);
     	efDescription = EjbDescriptionFactory.createFactory(clDescription);
-    	fService = EjbGeoServiceFactory.factory(clService);
+    	efService = EjbGeoServiceFactory.factory(cService);
+    	efMap = EjbGeoMapFactory.factory(cLang, cMap);
+    	efView = EjbGeoViewFactory.factory(cView);
 	}
 	
 	protected void reloadServices()
 	{
-		services = fGeo.all(clService);
+		services = fGeo.all(cService);
 	}
 	
 	public void addService() throws UtilsIntegrityException, InstantiationException, IllegalAccessException
 	{
-		service = fService.build("ocde", "http");
+		service = efService.build("ocde", "http");
 		
 		service.setName(efLang.createEmpty(langKeys));
 		service.setDescription(efDescription.createEmpty(langKeys));
@@ -78,7 +89,7 @@ public class AbstractMapServiceBean <L extends UtilsLang,D extends UtilsDescript
 
 	public void selectService() throws UtilsNotFoundException
 	{
-		service = fGeo.load(clService,service);
+		service = fGeo.load(cService,service);
 		logger.info("selectService "+service);
 		reloadLayer();
 		layer=null;
@@ -92,5 +103,40 @@ public class AbstractMapServiceBean <L extends UtilsLang,D extends UtilsDescript
 		reloadLayer();
 	}
 	
-	protected void reloadLayer(){}
+	
+	// LAYER
+	protected void reloadLayer()
+	{
+		service = fGeo.load(cService, service);
+		layers = service.getLayer();
+		logger.info("#Layer:"+layers.size());
+	}
+	
+	public void addLayer() throws UtilsIntegrityException, UtilsNotFoundException, InstantiationException, IllegalAccessException
+	{
+		logger.info("addLayer ");
+		layer = efLayer.build("", service,langKeys);
+		
+		layer.setDescription(efDescription.createEmpty(langKeys));
+	}
+	
+	public void selectLayer() throws UtilsNotFoundException
+	{
+		logger.info("selectLayer "+layer);
+		layer = fGeo.load(cLayer,layer);
+		
+		VIEW view = efView.build();
+		view.setLayer(layer);
+		
+		map = efMap.build();
+		map.getViews().add(view);
+	}
+	
+	public void saveLayer() throws UtilsContraintViolationException, UtilsLockingException, UtilsNotFoundException
+	{
+		logger.info("saveLayer "+layer);
+		layer.setService(fGeo.find(cService, layer.getService().getId()));
+		layer = fGeo.save(layer);
+		reloadLayer();
+	}
 }
--- util/src/main/java/org/geojsf/factory/ejb/EjbGeoMapFactory.java
+++ util/src/main/java/org/geojsf/factory/ejb/EjbGeoMapFactory.java
@@ -1,13 +1,12 @@
 package org.geojsf.factory.ejb;
 
-import net.sf.ahtutils.exception.ejb.UtilsIntegrityException;
 import net.sf.ahtutils.factory.ejb.status.EjbLangFactory;
 import net.sf.ahtutils.model.interfaces.status.UtilsDescription;
 import net.sf.ahtutils.model.interfaces.status.UtilsLang;
 
 import org.geojsf.interfaces.model.GeoJsfLayer;
-import org.geojsf.interfaces.model.GeoJsfService;
 import org.geojsf.interfaces.model.GeoJsfMap;
+import org.geojsf.interfaces.model.GeoJsfService;
 import org.geojsf.interfaces.model.GeoJsfView;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -16,35 +15,50 @@ public class EjbGeoMapFactory<L extends UtilsLang,D extends UtilsDescription,SER
 {
 	final static Logger logger = LoggerFactory.getLogger(EjbGeoMapFactory.class);
 	
-	final Class<MAP> cView;
+	final Class<MAP> cMap;
 	private EjbLangFactory<L> fLang;
     
     public static <L extends UtilsLang,D extends UtilsDescription,SERVICE extends GeoJsfService<L,D,SERVICE,LAYER,MAP,VIEW>, LAYER extends GeoJsfLayer<L,D,SERVICE,LAYER,MAP,VIEW>,MAP extends GeoJsfMap<L,D,SERVICE,LAYER,MAP,VIEW>, VIEW extends GeoJsfView<L,D,SERVICE,LAYER,MAP,VIEW>>
-    	EjbGeoMapFactory<L,D,SERVICE,LAYER,MAP,VIEW> factory(final Class<L> cLang,final Class<MAP> cView)
+    	EjbGeoMapFactory<L,D,SERVICE,LAYER,MAP,VIEW> factory(final Class<L> cLang,final Class<MAP> cMap)
     {
-        return new EjbGeoMapFactory<L,D,SERVICE,LAYER,MAP,VIEW>(cLang,cView);
+        return new EjbGeoMapFactory<L,D,SERVICE,LAYER,MAP,VIEW>(cLang,cMap);
     }
     
-    public EjbGeoMapFactory(final Class<L> cLang,final Class<MAP> cView)
+    public EjbGeoMapFactory(final Class<L> cLang,final Class<MAP> cMap)
     {
-        this.cView = cView;
+        this.cMap = cMap;
         fLang = EjbLangFactory.createFactory(cLang);
     } 
 	
-	public MAP create(String code, int zoom, double x, double y, String[] langKeys) throws UtilsIntegrityException
+	public MAP create(String code, int zoom, double x, double y, String[] langKeys)
 	{
-		MAP ejb;
-		try
-		{
-			ejb = cView.newInstance();
+		MAP ejb = build();
+		try {
 			ejb.setName(fLang.createEmpty(langKeys));
+		} catch (InstantiationException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IllegalAccessException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
-		catch (InstantiationException e) {throw new UtilsIntegrityException(e.getMessage());}
-		catch (IllegalAccessException e) {throw new UtilsIntegrityException(e.getMessage());}
 		ejb.setCode(code);
 		ejb.setZoom(zoom);
 		ejb.setLat(x);
 		ejb.setLon(y);
+        return ejb;
+    }
+	
+	public MAP build()
+	{
+		MAP ejb = null;
+		try
+		{
+			ejb = cMap.newInstance();
+		}
+		catch (InstantiationException e) {e.printStackTrace();}
+		catch (IllegalAccessException e) {e.printStackTrace();}
+		
         return ejb;
     }
 }
