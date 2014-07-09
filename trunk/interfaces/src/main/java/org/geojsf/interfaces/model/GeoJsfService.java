e2b406f2d480ef320610088e3eb6315bc60feaf8
thorsten-k <thorsten-k@d7d7bd7b-d12e-478d-8a14-7487b8a8350c>
2014-07-09 12:03:31 +0000

    Marker-Interface EjbSaveable added

--- trunk/interfaces/src/main/java/org/geojsf/interfaces/model/GeoJsfService.java
+++ trunk/interfaces/src/main/java/org/geojsf/interfaces/model/GeoJsfService.java
@@ -2,11 +2,11 @@ package org.geojsf.interfaces.model;
 
 import java.util.List;
 
+import net.sf.ahtutils.interfaces.model.behaviour.EjbSaveable;
 import net.sf.ahtutils.model.interfaces.status.UtilsDescription;
 import net.sf.ahtutils.model.interfaces.status.UtilsLang;
 import net.sf.ahtutils.model.interfaces.with.EjbWithCode;
 import net.sf.ahtutils.model.interfaces.with.EjbWithDescription;
-import net.sf.ahtutils.model.interfaces.with.EjbWithId;
 import net.sf.ahtutils.model.interfaces.with.EjbWithLang;
 
 public interface GeoJsfService<L extends UtilsLang,
@@ -16,7 +16,7 @@ public interface GeoJsfService<L extends UtilsLang,
 								MAP extends GeoJsfMap<L,D,SERVICE,LAYER,MAP,VIEW,VP>,
 								VIEW extends GeoJsfView<L,D,SERVICE,LAYER,MAP,VIEW,VP>,
 								VP extends GeoJsfViewPort<L,D,SERVICE,LAYER,MAP,VIEW,VP>>
-			extends EjbWithId,EjbWithCode,EjbWithLang<L>,EjbWithDescription<D>
+			extends EjbSaveable,EjbWithCode,EjbWithLang<L>,EjbWithDescription<D>
 {
 	public static final String extractId = "geoJsfService";
 	
