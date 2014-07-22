5e34b512a4f6584f96b5d7b768d3504543ed2044
hhemmer <hhemmer@d7d7bd7b-d12e-478d-8a14-7487b8a8350c>
2014-07-22 09:43:52 +0000

    Added viewport information to MapAjaxEvent (GEO-69)

--- trunk/component/src/main/java/org/geojsf/event/MapAjaxEvent.java
+++ trunk/component/src/main/java/org/geojsf/event/MapAjaxEvent.java
@@ -7,6 +7,7 @@ import javax.faces.event.AjaxBehaviorListener;
 import javax.faces.event.FacesListener;
 
 import org.geojsf.xml.geojsf.Coordinate;
+import org.geojsf.xml.geojsf.ViewPort;
 
 /**
  * This type of event is used in combination with the <f:ajax> tag.
@@ -19,6 +20,7 @@ public class MapAjaxEvent extends AjaxBehaviorEvent
 {
 	private static final long serialVersionUID = 1L;
 	private Coordinate coordinate;
+	private ViewPort viewPort;
 	private Float scale;
 	
 	public MapAjaxEvent(UIComponent component, ClientBehavior behavior)
@@ -32,6 +34,17 @@ public class MapAjaxEvent extends AjaxBehaviorEvent
 		coordinate = new Coordinate();
 		coordinate.setLat(new Double(lat));
 		coordinate.setLon(new Double(lon));
+	}
+	
+	public void setViewport(String lat, String lon, String top, String bottom, String left, String right)
+	{
+		viewPort = new org.geojsf.xml.geojsf.ViewPort();
+		viewPort.setLat(new Double(lat));
+		viewPort.setLon(new Double(lon));
+		viewPort.setTop(new Double(top));
+		viewPort.setBottom(new Double(bottom));
+		viewPort.setLeft(new Double(left));
+		viewPort.setRight(new Double(right));
 	}
 	
 	public Coordinate getCoordinate()
@@ -49,4 +62,13 @@ public class MapAjaxEvent extends AjaxBehaviorEvent
 
 	@Override public boolean isAppropriateListener(FacesListener faceslistener) {return (faceslistener instanceof AjaxBehaviorListener);}
     @Override public void processListener(FacesListener faceslistener) {((AjaxBehaviorListener) faceslistener).processAjaxBehavior(this);}
+
+	public ViewPort getViewPort() {
+		return viewPort;
+	}
+
+	public void setViewPort(ViewPort viewPort) {
+		this.viewPort = viewPort;
+	}
+
 }
