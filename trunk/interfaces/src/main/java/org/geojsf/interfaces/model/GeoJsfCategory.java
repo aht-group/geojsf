40e87d9f1fd104ba2a585d4cd9f8240e08d024ed
thorsten-k <thorsten-k@d7d7bd7b-d12e-478d-8a14-7487b8a8350c>
2014-08-15 04:15:42 +0000

    preparing adding Categories

--- trunk/interfaces/src/main/java/org/geojsf/interfaces/model/GeoJsfCategory.java
+++ trunk/interfaces/src/main/java/org/geojsf/interfaces/model/GeoJsfCategory.java
@@ -3,21 +3,21 @@ package org.geojsf.interfaces.model;
 import java.util.List;
 
 import net.sf.ahtutils.interfaces.model.behaviour.EjbSaveable;
-import net.sf.ahtutils.model.interfaces.crud.EjbRemoveable;
 import net.sf.ahtutils.model.interfaces.status.UtilsDescription;
 import net.sf.ahtutils.model.interfaces.status.UtilsLang;
 import net.sf.ahtutils.model.interfaces.with.EjbWithCode;
 import net.sf.ahtutils.model.interfaces.with.EjbWithDescription;
 import net.sf.ahtutils.model.interfaces.with.EjbWithLang;
 
-public interface GeoJsfService<L extends UtilsLang,
+public interface GeoJsfCategory<L extends UtilsLang,
 								D extends UtilsDescription,
+								C extends GeoJsfCategory<L,D,C,SERVICE,LAYER,MAP,VIEW,VP>,
 								SERVICE extends GeoJsfService<L,D,SERVICE,LAYER,MAP,VIEW,VP>,
 								LAYER extends GeoJsfLayer<L,D,SERVICE,LAYER,MAP,VIEW,VP>,
 								MAP extends GeoJsfMap<L,D,SERVICE,LAYER,MAP,VIEW,VP>,
 								VIEW extends GeoJsfView<L,D,SERVICE,LAYER,MAP,VIEW,VP>,
 								VP extends GeoJsfViewPort<L,D,SERVICE,LAYER,MAP,VIEW,VP>>
-			extends EjbSaveable,EjbWithCode,EjbWithLang<L>,EjbWithDescription<D>,EjbRemoveable
+			extends EjbSaveable,EjbWithCode,EjbWithLang<L>,EjbWithDescription<D>
 {
 	public static final String extractId = "geoJsfService";
 	
