5cf106df805f1c7f39366d0867d9906097837e29
thorsten-k <thorsten-k@d7d7bd7b-d12e-478d-8a14-7487b8a8350c>
2015-02-19 08:41:22 +0000

    geoserver documentation updated

--- trunk/doc/src/main/java/org/geojsf/doc/GeoJsfDocumentation.java
+++ trunk/doc/src/main/java/org/geojsf/doc/GeoJsfDocumentation.java
@@ -2,6 +2,8 @@ package org.geojsf.doc;
 
 public interface GeoJsfDocumentation
 {
+	public static final String prefixListingDir = "listing.geojsf";
+	
 	public static String keyBaseDocDir = "org.geojsf.doc.dir.base";
 	
 	public static String keyTranslationFile = "net.sf.ahtutils.doc.file.translation";
@@ -9,4 +11,8 @@ public interface GeoJsfDocumentation
 	public static String keyRoles = "net.sf.ahtutils.doc.file.roles";
 	
 	public static String msgGeoAdmin = "msg.geojsf/admin.xml";
+	
+	public static final String lstInstGeoserverUser = "admin/installation/geoserver/user.txt";
+	public static final String listingInstallGeoserverExtract = "admin/installation/geoserver/extract.txt";
+	public static final String lstInstGeoserverSrvPrep = "admin/installation/geoserver/service/preparation.txt";
 }
