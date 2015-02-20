14066858fccffa0396ea784126906b2dd18f1eb2
thorsten-k <thorsten-k@d7d7bd7b-d12e-478d-8a14-7487b8a8350c>
2015-02-20 10:14:56 +0000

    PointFactory added

---
+++ trunk/util/src/main/java/org/geojsf/factory/wkt/PointFactory.java
@@ -0,0 +1,36 @@
+package org.geojsf.factory.wkt;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.vividsolutions.jts.geom.GeometryFactory;
+import com.vividsolutions.jts.geom.Point;
+import com.vividsolutions.jts.geom.PrecisionModel;
+
+public class PointFactory
+{
+	final static Logger logger = LoggerFactory.getLogger(PointFactory.class);
+	
+	private GeometryFactory gf;
+	
+	public PointFactory()
+	{
+		PrecisionModel pm = new PrecisionModel(PrecisionModel.FLOATING);
+		gf = new GeometryFactory(pm,4326);
+	}
+	
+	public Point build(org.geojsf.xml.geojsf.Coordinate coordinate)
+	{
+		return build(coordinate.getLat(),coordinate.getLon());
+	}
+	
+    public Point build(double lat, double lon)
+    {
+    	com.vividsolutions.jts.geom.Coordinate coordinate = new com.vividsolutions.jts.geom.Coordinate();
+		coordinate.x = lat;
+		coordinate.y = lon;
+		
+		return gf.createPoint(coordinate);
+    } 
+    
+}
