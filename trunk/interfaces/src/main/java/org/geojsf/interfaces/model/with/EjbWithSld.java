557933d1183b3534bd10a60b6d09e36b1e79c228
thorsten-k <thorsten-k@d7d7bd7b-d12e-478d-8a14-7487b8a8350c>
2015-03-10 13:51:29 +0000

    GeoJsfSldRule extended

--- trunk/interfaces/src/main/java/org/geojsf/interfaces/model/with/EjbWithSld.java
+++ trunk/interfaces/src/main/java/org/geojsf/interfaces/model/with/EjbWithSld.java
@@ -6,11 +6,13 @@ import net.sf.ahtutils.model.interfaces.status.UtilsStatus;
 import net.sf.ahtutils.model.interfaces.with.EjbWithId;
 
 import org.geojsf.interfaces.model.sld.GeoJsfSld;
+import org.geojsf.interfaces.model.sld.GeoJsfSldRule;
 
 public interface EjbWithSld <L extends UtilsLang,
 							D extends UtilsDescription,
 							TYPE extends UtilsStatus<TYPE,L,D>,
-							SLD extends GeoJsfSld<L,D,TYPE,SLD>>
+							SLD extends GeoJsfSld<L,D,TYPE,SLD,RULE>,
+							RULE extends GeoJsfSldRule<L,D,TYPE,SLD,RULE>>
 						extends EjbWithId
 {
 	SLD getSld();
