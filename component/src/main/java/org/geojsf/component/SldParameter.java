2c9ec2bccda63beafcd81886d708b607bbad3239
hhemmer <hhemmer@web.de>
2015-03-10 07:05:43 +0000

    Added SqlParameter component (GEO-152)

---
+++ component/src/main/java/org/geojsf/component/SldParameter.java
@@ -0,0 +1,54 @@
+package org.geojsf.component;
+
+import java.io.IOException;
+import java.util.Map;
+
+import javax.faces.component.FacesComponent;
+import javax.faces.component.UIComponentBase;
+import javax.faces.context.FacesContext;
+import javax.faces.event.AbortProcessingException;
+import javax.faces.event.ComponentSystemEvent;
+import javax.faces.event.ListenerFor;
+import javax.faces.event.PostAddToViewEvent;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@FacesComponent(value="org.geojsf.component.SdlParameter")
+@ListenerFor(systemEventClass=PostAddToViewEvent.class)
+public class SldParameter extends UIComponentBase
+{
+	final static Logger logger = LoggerFactory.getLogger(SldParameter.class);
+	
+	private static enum Attribute {value}
+	
+	private String value;
+	
+	@Override
+	public void processEvent(ComponentSystemEvent event) throws AbortProcessingException
+	{
+		if(event instanceof PostAddToViewEvent)
+		{
+			
+		}
+		super.processEvent(event);
+	}
+	
+	@Override
+	public void encodeAll(FacesContext ctx) throws IOException
+	{
+		Map<String,Object> map = this.getAttributes();
+		this.value   = (String) map.get(Attribute.value.toString());
+		String command = "<script>GeoJSF.updateSdlParams('" +value +"');</script>";
+		ctx.getResponseWriter().write(command);
+		logger.info("Styled Layer Description (SDL) Parameter updated in Map. JavaScript Command: " +command);
+	}
+
+	public void setValue(String value) {this.value = value;}
+	public String getValue() {return value;}
+
+	@Override
+	public String getFamily() {
+		return null;
+	}
+}
--- component/src/main/resources/META-INF/resources/js.geojsf/GeoJSF.js
+++ component/src/main/resources/META-INF/resources/js.geojsf/GeoJSF.js
@@ -327,6 +327,20 @@ var GeoJSF = {
 			 console.log("Merging new SqlViewParameter parameter: " +params.viewparams);
 		},
 		
+		updateSqlViewParams : function(parameters)
+		{
+			 console.log("SDL Parameter Update requested.");
+			 var params = {};
+			 params.env = parameters;
+			 if (GeoJSF.map.layers)
+				{
+					for (var i = GeoJSF.map.layers.length - 1; i >= 0; i--) {
+					    GeoJSF.map.layers[i].mergeNewParams(params);
+					}
+				}
+			 console.log("Merging new SdlParameter parameter: " +params.viewparams);
+		},
+		
 		switchLayerClassic : function(layerId)
 		{
 			 // This is the pure JSF based approach, not having an 'oncomplete' method:	 
