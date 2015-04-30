214addb1a71e192dbbab2202b32fc28e57480837
hhemmer <hhemmer@web.de>
2015-04-30 07:35:30 +0000

    Viewport is now zoomed to scale or (if restricted) to closest zoom level defined by allowed scales

--- component/src/main/resources/META-INF/resources/js.geojsf/viewport.js
+++ component/src/main/resources/META-INF/resources/js.geojsf/viewport.js
@@ -1,7 +1,17 @@
 var GeoJsfViewport = {	
 
-		center : function(lon,lat,zoom)
+		center : function(lon,lat,zoom,isScale)
 		{
-			GeoJSF.map.setCenter(new OpenLayers.LonLat(lon, lat), zoom, true, true);
+			if (isScale)
+				{
+					GeoJSF.map.setCenter(new OpenLayers.LonLat(lon, lat));
+					GeoJSF.map.zoomToScale(zoom);
+					console.log("Centered and zoomed to scale of " +zoom);
+				}
+			else
+				{
+					GeoJSF.map.setCenter(new OpenLayers.LonLat(lon, lat), zoom, true, true);
+					console.log("Centered and zoomed to zoom level of " +zoom);
+				}
 		},
 };
