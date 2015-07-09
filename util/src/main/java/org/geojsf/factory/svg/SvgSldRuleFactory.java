0b411c4530ba230ac995d35869b660c62a8fb8f3
thorsten-k <t.kisner@web.de>
2015-07-09 18:08:26 +0000

    dynamic size of SvgPointSymbolizer

--- util/src/main/java/org/geojsf/factory/svg/SvgSldRuleFactory.java
+++ util/src/main/java/org/geojsf/factory/svg/SvgSldRuleFactory.java
@@ -29,13 +29,30 @@ public class SvgSldRuleFactory<L extends UtilsLang,
 {
 	final static Logger logger = LoggerFactory.getLogger(SvgSldRuleFactory.class);
 		
+	private DOMImplementation impl;
+	
+	public SvgSldRuleFactory()
+	{
+		impl = SVGDOMImplementation.getDOMImplementation();
+	}
+	
+    public static <L extends UtilsLang,
+					D extends UtilsDescription,
+					TYPE extends UtilsStatus<TYPE,L,D>,
+					STYLE extends UtilsStatus<STYLE,L,D>,
+					SLD extends GeoJsfSld<L,D,TYPE,STYLE,SLD,RULE,TEMPLATE>,
+					RULE extends GeoJsfSldRule<L,D,TYPE,STYLE,SLD,RULE,TEMPLATE>,
+					TEMPLATE extends GeoJsfSldTemplate<L,D,TYPE,STYLE,TEMPLATE>>
+    	SvgSldRuleFactory<L,D,TYPE,STYLE,SLD,RULE,TEMPLATE> factory()
+	{
+	    return new SvgSldRuleFactory<L,D,TYPE,STYLE,SLD,RULE,TEMPLATE>();
+	}
     
 	public static SVGGraphics2D build()
 	{
 		// Create an SVG document.
 	    DOMImplementation impl = SVGDOMImplementation.getDOMImplementation();
-	    String svgNS = SVGDOMImplementation.SVG_NAMESPACE_URI;
-	    SVGDocument doc = (SVGDocument) impl.createDocument(svgNS, "svg", null);
+	    SVGDocument doc = (SVGDocument) impl.createDocument(SVGDOMImplementation.SVG_NAMESPACE_URI, "svg", null);
 
 	    // Create a converter for this document.
 	    SVGGraphics2D g = new SVGGraphics2D(doc);
@@ -51,6 +68,30 @@ public class SvgSldRuleFactory<L extends UtilsLang,
 	    g.setPaint(Color.blue);
 	    g.fill(circle);
 	    g.setSVGCanvasSize(new Dimension(180, 50));
+	    
+	    return g;
+	}
+	
+	public SVGGraphics2D build(int canvasSize, RULE rule)
+	{
+		int size = 5;
+		if(rule.getSize()!=null){size = rule.getSize();}
+		
+	    SVGDocument doc = (SVGDocument) impl.createDocument(SVGDOMImplementation.SVG_NAMESPACE_URI, "svg", null);
+	    SVGGraphics2D g = new SVGGraphics2D(doc);
+
+	    double cS = canvasSize; double s = size;
+	    double low = (cS - s)/2;
+	    
+	    logger.info("Canvas: "+canvasSize+" low:"+low+" size:"+size);
+	    
+	    // Do some drawing.
+	    Shape circle = new Ellipse2D.Double(low, low, size, size);
+//	    g.setPaint(Color.decode(rule.getColor()));
+	    g.setPaint(Color.red);
+	    g.fill(circle);
+	    
+	    g.setSVGCanvasSize(new Dimension(canvasSize, canvasSize));
 	    
 	    return g;
 	}
