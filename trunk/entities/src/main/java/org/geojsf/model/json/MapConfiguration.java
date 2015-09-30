62e108586291511f2c2a85c2d35572fc4cb32129
hhemmer <hhemmer@d7d7bd7b-d12e-478d-8a14-7487b8a8350c>
2015-09-30 05:23:01 +0000

    Added JSON data model for map configuration

---
+++ trunk/entities/src/main/java/org/geojsf/model/json/MapConfiguration.java
@@ -0,0 +1,37 @@
+package org.geojsf.model.json;
+
+/**
+ *
+ * @author helgehemmer
+ */
+public class MapConfiguration {
+    
+    private String     divContainer;
+    private Integer    height;
+    private Integer    width;
+
+    public String getDivContainer() {
+        return divContainer;
+    }
+
+    public void setDivContainer(String divContainer) {
+        this.divContainer = divContainer;
+    }
+
+    public Integer getHeight() {
+        return height;
+    }
+
+    public void setHeight(Integer height) {
+        this.height = height;
+    }
+
+    public Integer getWidth() {
+        return width;
+    }
+
+    public void setWidth(Integer width) {
+        this.width = width;
+    }
+    
+}
