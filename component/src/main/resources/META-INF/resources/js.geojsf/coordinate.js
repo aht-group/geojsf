f87d3b64b0cf3c50d97f33e5d0d844be2020d96e
Helge Hemmer <hhemmer@web.de>
2016-02-22 10:07:40 +0100

    Fixed lon lat order in coordinates control showing the position on the map itself (GEO-172)

--- component/src/main/resources/META-INF/resources/js.geojsf/coordinate.js
+++ component/src/main/resources/META-INF/resources/js.geojsf/coordinate.js
@@ -1,12 +1,21 @@
-var GeoJsfCoordinate = {	
+var GeoJsfCoordinate = {
+	
+	myFormat : function(dgts)
+	{
+	  return (
+		function(coord1) {
+			var coord2 = [coord1[1], coord1[0]]; 
+		  return ol.coordinate.toStringXY(coord2,dgts);
+	  });        
+	},
 
-		addCoordinatesControl : function(position, precision)
-		{
-                    // Please use CSS to configure appearance
-                    var mousePosition = new ol.control.MousePosition({
-                                           coordinateFormat: ol.coordinate.createStringXY(precision),
-                                           projection      : 'EPSG:4326'
-                                        });
-                    GeoJSF.map.addControl(mousePosition);
-		},
+	addCoordinatesControl : function(position, precision)
+	{
+				// Please use CSS to configure appearance
+				var mousePosition = new ol.control.MousePosition({
+									   coordinateFormat:	GeoJsfCoordinate.myFormat(2),
+									   projection      : 'EPSG:4326'
+									});
+				GeoJSF.map.addControl(mousePosition);
+	},
 };
