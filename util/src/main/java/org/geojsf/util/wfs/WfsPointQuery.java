bbe20daf53f95e000bddee1f86a8004d4c37f281
Thorsten <t.kisner@web.de>
2016-02-25 07:20:51 +0100

    logging improved

--- util/src/main/java/org/geojsf/util/wfs/WfsPointQuery.java
+++ util/src/main/java/org/geojsf/util/wfs/WfsPointQuery.java
@@ -47,7 +47,6 @@ import net.sf.ahtutils.interfaces.model.status.UtilsDescription;
 import net.sf.ahtutils.interfaces.model.status.UtilsLang;
 import net.sf.ahtutils.interfaces.model.status.UtilsStatus;
 import net.sf.ahtutils.model.interfaces.with.EjbWithId;
-import net.sf.exlp.util.xml.JDomUtil;
 import net.sf.exlp.util.xml.JaxbUtil;
 
 @SuppressWarnings("rawtypes")
