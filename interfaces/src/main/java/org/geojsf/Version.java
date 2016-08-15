35d61b747c874151a89ad996d16aa47b5dc2d454
Thorsten <t.kisner@web.de>
2016-08-15 09:49:30 +0200

    artifact added to Version

--- interfaces/src/main/java/org/geojsf/Version.java
+++ interfaces/src/main/java/org/geojsf/Version.java
@@ -2,5 +2,9 @@ package org.geojsf;
 
 public class Version
 {
-
+	public static final String mvnGeoJsfEntities = "org.geojsf:geojsf-entities:";
+	public static final String mvnGeoJsfInterfaces = "org.geojsf:geojsf-interfaces:";
+	public static final String mvnGeoJsfSpi = "org.geojsf:geojsf-spi:";
+	public static final String mvnGeoJsfEjb = "org.geojsf:geojsf-ejb:";
+	public static final String mvnGeoJsfUtil = "org.geojsf:geojsf-util:";
 }
