44eaf47a091fc42001bfbdff58bcb6381c69041e
Thorsten <t.kisner@web.de>
2017-07-04 13:29:45 +0200

    StationCode fixed

--- interfaces/src/main/java/org/geojsf/interfaces/model/obervation/station/GeoStationCode.java
+++ interfaces/src/main/java/org/geojsf/interfaces/model/obervation/station/GeoStationCode.java
@@ -1,21 +1,12 @@
 package org.geojsf.interfaces.model.obervation.station;
 
-import java.util.List;
-
-import org.geojsf.interfaces.model.with.EjbWithGeometry;
-
 import net.sf.ahtutils.interfaces.model.status.UtilsDescription;
 import net.sf.ahtutils.interfaces.model.status.UtilsLang;
 import net.sf.ahtutils.interfaces.model.status.UtilsStatus;
 import net.sf.ahtutils.interfaces.model.with.code.EjbWithCode;
-import net.sf.ahtutils.model.interfaces.with.EjbWithDescription;
 import net.sf.ahtutils.model.interfaces.with.EjbWithId;
-import net.sf.ahtutils.model.interfaces.with.EjbWithLang;
 
-import com.vividsolutions.jts.geom.Point;
-
-public interface GeoStationCode<L extends UtilsLang,
-							D extends UtilsDescription,
+public interface GeoStationCode<L extends UtilsLang, D extends UtilsDescription,
 							STATION extends GeoStation<L,D,STATION,TYPE,SUBTYPE,SCHEME,CODE,CAP,CAPT,CAPS>,
 							TYPE extends UtilsStatus<TYPE,L,D>,
 							SUBTYPE extends UtilsStatus<SUBTYPE,L,D>,
@@ -24,19 +15,11 @@ public interface GeoStationCode<L extends UtilsLang,
 							CAP extends GeoStationCapability<L,D,STATION,TYPE,SUBTYPE,SCHEME,CODE,CAP,CAPT,CAPS>,
 							CAPT extends UtilsStatus<CAPT,L,D>,
 							CAPS extends UtilsStatus<CAPS,L,D>>
-			extends EjbWithId,EjbWithCode,EjbWithLang<L>,EjbWithDescription<D>,EjbWithGeometry<Point>
+			extends EjbWithId,EjbWithCode
 {
 	public enum Attributes{capabilities}
 	
 	SCHEME getScheme();
 	void setScheme(SCHEME scheme);
 	
-	TYPE getType();
-	void setType(TYPE type);
-	
-	SUBTYPE getSubType();
-	void setSubType(SUBTYPE subType);
-	
-	List<CAP> getCapabilities();
-	void setCapabilities(List<CAP> capabilities);
 }
