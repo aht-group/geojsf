71a9caf8ac5c9f7069ce700489bfad9476a70d38
Thorsten-K <t.kisner@web.de>
2018-04-04 13:46:12 +0200

    TxtPointFactory improved

--- util/src/main/java/org/geojsf/factory/txt/TxtCoordinateFactory.java
+++ util/src/main/java/org/geojsf/factory/txt/TxtCoordinateFactory.java
@@ -7,6 +7,8 @@ import org.geojsf.model.xml.geojsf.Coordinate;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import com.vividsolutions.jts.geom.Point;
+
 public class TxtCoordinateFactory
 {
 	final static Logger logger = LoggerFactory.getLogger(TxtCoordinateFactory.class);
@@ -24,6 +26,15 @@ public class TxtCoordinateFactory
 		StringBuffer sb = new StringBuffer();
 		sb.append("lat:").append(df.format(coordinate.getLat()));
 		sb.append(" lon:").append(df.format(coordinate.getLon()));
+		return sb.toString();
+	}
+	
+	public String build(Point point)
+	{
+		StringBuffer sb = new StringBuffer();
+		sb.append("lat:").append(df.format(point.getX()));
+		sb.append(" lon:").append(df.format(point.getY()));
+		sb.append(" ... we need to verify lat/log");
 		return sb.toString();
 	}
 }
