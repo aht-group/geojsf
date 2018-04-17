3f1aa62094f9c8c4f6e54175eb4a3aaa7ea75dd0
Thorsten-K <t.kisner@web.de>
2018-04-17 10:45:10 +0200

    entities added

--- doc/src/main/java/org/geojsf/doc/GeoJsfDocumentation.java
+++ doc/src/main/java/org/geojsf/doc/GeoJsfDocumentation.java
@@ -7,6 +7,7 @@ public interface GeoJsfDocumentation
 	public static final String geojsfPath = "utils"+File.separator+"geojsf"+File.separator;
 	public static final String prefixListingDir = "listing.geojsf";
 	
+	public static final String entities = "geojsf/msg/admin/entities.xml";
 	public static final String entitiesPrefix = "geojsf/msg/admin/entitiesPrefix.xml";
 	public static String keyBaseDocDir = "org.geojsf.doc.dir.base";
 	
