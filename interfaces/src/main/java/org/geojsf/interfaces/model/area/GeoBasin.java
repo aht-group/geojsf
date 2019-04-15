7be5745a7adc0d7719fae32b8814de836b5b44df
Thorsten-K <t.kisner@web.de>
2019-04-15 05:20:32 +0200

    basins improved

--- interfaces/src/main/java/org/geojsf/interfaces/model/area/GeoBasin.java
+++ interfaces/src/main/java/org/geojsf/interfaces/model/area/GeoBasin.java
@@ -7,15 +7,16 @@ import net.sf.ahtutils.model.interfaces.with.EjbWithDescription;
 import net.sf.ahtutils.model.interfaces.with.EjbWithId;
 import net.sf.ahtutils.model.interfaces.with.EjbWithLang;
 
+import java.io.Serializable;
+
 import org.geojsf.interfaces.model.with.EjbWithGeometry;
 
 import com.vividsolutions.jts.geom.MultiPolygon;
 
-public interface GeoBasin<L extends UtilsLang,
-							D extends UtilsDescription,
+public interface GeoBasin<L extends UtilsLang, D extends UtilsDescription,
 							BASIN extends GeoBasin<L,D,BASIN,MODEL>,
 							MODEL extends UtilsStatus<MODEL,L,D>>
-			extends EjbWithId,EjbWithLang<L>,EjbWithDescription<D>,EjbWithGeometry<MultiPolygon>
+			extends Serializable,EjbWithId,EjbWithLang<L>,EjbWithDescription<D>,EjbWithGeometry<MultiPolygon>
 {
 	MODEL getModel();
 	void setModel(MODEL model);
