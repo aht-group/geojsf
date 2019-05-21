75d73a8b9141af62fb81a2f9fc07b3d50622c993
Thorsten-K <t.kisner@web.de>
2019-05-21 15:13:03 +0200

    JsonData added

---
+++ interfaces/src/main/java/org/geojsf/interfaces/model/json/GeoJsfJsonData.java
@@ -0,0 +1,24 @@
+package org.geojsf.interfaces.model.json;
+
+import java.io.Serializable;
+
+import net.sf.ahtutils.interfaces.model.behaviour.EjbSaveable;
+import net.sf.ahtutils.interfaces.model.crud.EjbPersistable;
+import net.sf.ahtutils.interfaces.model.crud.EjbRemoveable;
+import net.sf.ahtutils.interfaces.model.status.UtilsDescription;
+import net.sf.ahtutils.interfaces.model.status.UtilsLang;
+
+public interface GeoJsfJsonData<L extends UtilsLang, D extends UtilsDescription,
+						   JQ extends GeoJsfJsonQuality<JQ,L,D,?>,
+						   JL extends GeoJsfJsonQuality<JL,L,D,?>
+						   >
+			extends Serializable,EjbRemoveable,EjbPersistable,EjbSaveable
+{
+	public enum Attributes{quality,level}
+	
+	JQ getQuality();
+	void setQuality(JQ quality);
+	
+	JL getLevel();
+	void setLevel(JL level);
+}
