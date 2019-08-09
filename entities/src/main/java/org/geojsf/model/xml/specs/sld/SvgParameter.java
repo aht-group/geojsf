c4f4fa5dcc6e8d59a47a74ee5bf0dda39da3f830
Thorsten-K <t.kisner@web.de>
2019-08-09 10:50:47 +0200

    location of XSD fixed

--- entities/src/main/java/org/geojsf/model/xml/specs/sld/SvgParameter.java
+++ entities/src/main/java/org/geojsf/model/xml/specs/sld/SvgParameter.java
@@ -74,9 +74,9 @@ public class SvgParameter
      * 
      * <p>
      * Objects of the following type(s) are allowed in the list
-     * {@link Literal }
-     * {@link String }
      * {@link Function }
+     * {@link String }
+     * {@link Literal }
      * 
      * 
      */
