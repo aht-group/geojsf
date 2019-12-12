48f4831bba82ce6028ed1e6897a69add3c410ae3
manandhar <manandhar@nbsm19.aht-group.com>
2019-12-12 15:05:39 +0100

    add feature to show data table selection on map

--- component/src/main/resources/META-INF/resources/js.geojsf/GeoJSF3.js
+++ component/src/main/resources/META-INF/resources/js.geojsf/GeoJSF3.js
@@ -36,8 +36,7 @@ var GeoJSF = {
 		feature: null,
 		vectorSource: null,
 		scaleValues: null,
-		featureCircleOnClick:null,
-		
+			
 		iconStyle: new ol.style.Style({
 					image: new ol.style.Icon({
 					  anchor: [0.5, 0.75],
@@ -156,7 +155,7 @@ var GeoJSF = {
 		 			        ],
 		 			oncomplete: function(xhr, status, args) {
 		 				  console.log('mapClick AJAX request sent. Resolution : ' +GeoJSF.map.getView().getResolution()+ ' ' +GeoJSF.map.getView().getProjection().getUnits())
-            			  GeoJSF.updateCircleOnClick(lonlat);
+            			  clickSearcher.updateCircleOnClick(lonlat);
 		 			}
 				});
 			} catch(e) {
@@ -572,10 +571,23 @@ var GeoJSF = {
 			}
 			finally {}
 			GeoJSF.secondRun();
+		}
+};
+
+var clickSearcher = {
+		radiusFactor : 15,
+		enable : false,	
+		featureCircleOnClick : null,
+		enableClickSearchFeature : false,
+		
+		init:function(radFactor){
+			this.radiusFactor = radFactor;
+			this.enable = true;
 		},
 		
 		 updateCircleOnClick: function(lonlat) {
-			var radius = Math.round(GeoJSF.map.getView().getResolution()*15).toString();
+		 	if(!this.enable){return;}
+			var radius = Math.round(GeoJSF.map.getView().getResolution()*this.radiusFactor).toString();
 			
 		    if (this.featureCircleOnClick && this.featureCircleOnClick!== 'null' && this.featureCircleOnClick !== "undefined") {
 		        GeoJSF.map.removeLayer(this.featureCircleOnClick);
@@ -599,4 +611,57 @@ var GeoJSF = {
         	});
         	GeoJSF.map.addLayer(this.featureCircleOnClick);
 		}
+};
+
+var dtSelectCallback = {
+    svgMarker: '<svg width="160" height="160" version="1.1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 160 160" viewPort="0 0 160 160" class="svgClass">' +
+        '<circle cx="30" cy="30" r="10" stroke="rgb(0, 191, 255)" stroke-width="1" fill="none" opacity="0.1">' +
+        '<animate attributeType="CSS" attributeName="stroke-width" from="1" to="30" dur="0.5s" begin="0s" repeatCount="indefinite" />' +
+        '<animate attributeType="CSS" attributeName="opacity" from="0.8" to="0.2" dur="0.5s" begin="0s" repeatCount="indefinite" />' +
+        '</circle>' +
+        '<circle cx="30" cy="30" r="10" fill="rgba(0,0,0,0.8)">' +
+        '</circle>' +
+        '</svg>',
+
+    lastMarker: '',
+
+    createMarker: function(lon, lat) {
+        this.removeMarker();
+        var marker = new ol.Feature({
+            geometry: new ol.geom.Point(ol.proj.fromLonLat([lon, lat])),
+        });
+        /*
+        marker.setStyle(new ol.style.Style({
+            image: new ol.style.Icon({
+                src: 'data:image/svg+xml;charset=utf-8,' + encodeURIComponent(this.svgMarker)
+            })
+        }));
+        
+        marker.setStyle(new ol.style.Style({
+            image: new ol.style.Icon(({
+                crossOrigin: 'anonymous',
+                src: '../geojsf/marker.png',
+                // the real size of your icon
+                size: [255, 300],
+                // the scale factor
+                scale: 0.2
+            }))
+        }));
+		*/
+        var vectorSource = new ol.source.Vector({
+            features: [marker]
+        });
+
+        this.lastMarker = new ol.layer.Vector({
+            source: vectorSource,
+        });
+        GeoJSF.map.addLayer(this.lastMarker);
+    },
+
+    removeMarker: function() {
+        if (this.lastMarker) {
+            GeoJSF.map.removeLayer(this.lastMarker);
+            this.lastMarker = '';
+        }
+    },
 };
