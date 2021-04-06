b7b6d349313d4663c74b133a19c7f7ec18387dff
Helge Hemmer <hemmer@aht-group.com>
2021-04-06 09:35:37 +0200

    Added locationtech model based Point builder

--- util/src/main/java/org/geojsf/factory/wkt/PointFactory.java
+++ util/src/main/java/org/geojsf/factory/wkt/PointFactory.java
@@ -40,15 +40,42 @@ public class PointFactory
 		return gf.createPoint(coordinate);
     }
     
+    public org.locationtech.jts.geom.Point buildLocationTechPoint(double lat, double lon)
+    {
+    	org.locationtech.jts.geom.Coordinate coordinate = new org.locationtech.jts.geom.Coordinate();
+	coordinate.y = lat;
+	coordinate.x = lon;
+	org.locationtech.jts.geom.PrecisionModel pm = new org.locationtech.jts.geom.PrecisionModel(org.locationtech.jts.geom.PrecisionModel.FLOATING);
+
+	return new org.locationtech.jts.geom.GeometryFactory(pm,4326).createPoint(coordinate);
+    }
+    
     public Point build(Wkt wkt) throws ParseException
     {
-		return build(wkt.getValue());
+	return build(wkt.getValue());
     }
     
     public Point build(String wkt) throws ParseException
     {
-		Geometry geometry = wktReader.read(wkt);
-		geometry.setSRID(4326);
-		return (Point)geometry;
+	Geometry geometry = wktReader.read(wkt);
+	geometry.setSRID(4326);
+	return (Point)geometry;
+    }
+    
+    public org.locationtech.jts.geom.Point buildLocationTechPoint(Wkt wkt) throws ParseException
+    {
+	return buildLocationTechPoint(wkt.getValue());
+    }
+    
+    public org.locationtech.jts.geom.Point buildLocationTechPoint(String wkt) throws ParseException
+    {
+	org.locationtech.jts.geom.Geometry geometry = null;
+	try {
+	    geometry = new org.locationtech.jts.io.WKTReader().read(wkt);
+	} catch (org.locationtech.jts.io.ParseException ex) {
+	    logger.error("Could not build Point from wkt String: " +ex.getMessage());
+	}
+	geometry.setSRID(4326);
+	return (org.locationtech.jts.geom.Point)geometry;
     }
 }
