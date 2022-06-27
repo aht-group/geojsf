2a17f7f8e78139357129335cf1799d276d7dd5bf
Thorsten-K <t.kisner@web.de>
2022-06-27 13:17:19 +0200

    changes of jeesl applied

--- interfaces/src/main/java/org/geojsf/interfaces/model/monitoring/GeoBushFire.java
+++ interfaces/src/main/java/org/geojsf/interfaces/model/monitoring/GeoBushFire.java
@@ -2,7 +2,7 @@ package org.geojsf.interfaces.model.monitoring;
 
 
 import org.geojsf.interfaces.model.with.EjbWithGeometry;
-import org.jeesl.interfaces.model.with.primitive.date.EjbWithRecord;
+import org.jeesl.interfaces.model.with.date.ju.EjbWithRecord;
 import org.jeesl.interfaces.model.with.primitive.number.EjbWithId;
 
 import com.vividsolutions.jts.geom.Point;
