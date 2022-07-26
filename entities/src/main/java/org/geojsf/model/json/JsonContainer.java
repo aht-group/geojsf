5b6c17785c5e085cadc778074ba48b68f8418b78
l@Sse pet3Rs3n <petersen@aht-group.com>
2022-07-26 15:47:08 +0200

    add JsonLocationArea2 + JsonLocationArea3

--- entities/src/main/java/org/geojsf/model/json/JsonContainer.java
+++ entities/src/main/java/org/geojsf/model/json/JsonContainer.java
@@ -1,9 +1,12 @@
 package org.geojsf.model.json;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.List;
 
 import org.geojsf.model.json.location.JsonLocationArea1;
+import org.geojsf.model.json.location.JsonLocationArea2;
+import org.geojsf.model.json.location.JsonLocationArea3;
 import org.geojsf.model.json.location.JsonLocationCountry;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
@@ -27,4 +30,14 @@ public class JsonContainer implements Serializable
 	private List<JsonLocationArea1> areas1;
 	public List<JsonLocationArea1> getAreas1() {return areas1;}
 	public void setAreas1(List<JsonLocationArea1> areas1) {this.areas1 = areas1;}
+	
+	@JsonProperty("areas2")
+	private List<JsonLocationArea2> areas2;
+	public List<JsonLocationArea2> getAreas2() {return areas2;}
+	public void setAreas2(List<JsonLocationArea2> areas2) {this.areas2 = areas2;}
+	
+	@JsonProperty("areas3")
+	private List<JsonLocationArea3> areas3;
+	public List<JsonLocationArea3> getAreas3() {return areas3;}
+	public void setAreas3(List<JsonLocationArea3> areas3) {this.areas3 = areas3;}
 }
