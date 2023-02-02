24473a522c0f2595222c363ab345f80bc94782ae
Thorsten-K <t.kisner@web.de>
2023-02-02 14:02:21 +0100

    Download Annotations added

--- interfaces/src/main/java/org/geojsf/interfaces/model/sld/GeoJsfSld.java
+++ interfaces/src/main/java/org/geojsf/interfaces/model/sld/GeoJsfSld.java
@@ -10,7 +10,11 @@ import org.jeesl.interfaces.model.system.locale.JeeslDescription;
 import org.jeesl.interfaces.model.system.locale.JeeslLang;
 import org.jeesl.interfaces.model.with.system.locale.EjbWithDescription;
 import org.jeesl.interfaces.model.with.system.locale.EjbWithLang;
+import org.jeesl.interfaces.qualifier.rest.option.DownloadJeeslAttributes;
+import org.jeesl.interfaces.qualifier.rest.option.DownloadJeeslDescription;
 
+@DownloadJeeslDescription
+@DownloadJeeslAttributes
 public interface GeoJsfSld<L extends JeeslLang, D extends JeeslDescription,
 						   SLDTEMPLATE extends GeoJsfSldTemplate<L,D>,
 						   TYPE extends GeoJsfSldType<L,D,TYPE,?>,
