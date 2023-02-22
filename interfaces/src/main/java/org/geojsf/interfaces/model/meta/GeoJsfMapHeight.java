12f800c65e7c9b81e6381af5776f0f9889b67cc0
Thorsten-K <t.kisner@web.de>
2023-02-22 13:19:17 +0100

    GeoJsfMapHeight added

--- interfaces/src/main/java/org/geojsf/interfaces/model/meta/GeoJsfMapHeight.java
+++ interfaces/src/main/java/org/geojsf/interfaces/model/meta/GeoJsfMapHeight.java
@@ -9,12 +9,19 @@ import org.jeesl.interfaces.model.system.locale.JeeslDescription;
 import org.jeesl.interfaces.model.system.locale.JeeslLang;
 import org.jeesl.interfaces.model.system.locale.status.JeeslStatus;
 import org.jeesl.interfaces.model.system.locale.status.JeeslStatusFixedCode;
+import org.jeesl.interfaces.model.system.locale.status.JeeslStatusWithSymbol;
+import org.jeesl.interfaces.qualifier.rest.option.DownloadJeeslAttributes;
+import org.jeesl.interfaces.qualifier.rest.option.DownloadJeeslData;
+import org.jeesl.interfaces.qualifier.rest.option.DownloadJeeslDescription;
 
+@DownloadJeeslDescription
+@DownloadJeeslAttributes
+@DownloadJeeslData
 public interface GeoJsfMapHeight <L extends JeeslLang, D extends JeeslDescription,
 									S extends JeeslStatus<L,D,S>,
 									G extends JeeslGraphic<?,?,?>>
 					extends Serializable,EjbPersistable,
-							JeeslStatus<L,D,S>,JeeslStatusFixedCode,
+							JeeslStatus<L,D,S>,JeeslStatusFixedCode,JeeslStatusWithSymbol,
 							EjbWithCodeGraphic<G>
 {
 	public static enum Code {h300, h400, h500}
