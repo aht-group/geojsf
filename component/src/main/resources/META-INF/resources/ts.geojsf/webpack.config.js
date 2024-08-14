8c2fae20fbcdac6b510a20c35df9c4b7d5d1a2c9
manandhar-aht <sachin.manandhar@aht-group.com>
2024-08-14 17:40:55 +0200

    fix node packaging issue

--- component/src/main/resources/META-INF/resources/ts.geojsf/webpack.config.js
+++ component/src/main/resources/META-INF/resources/ts.geojsf/webpack.config.js
@@ -1,31 +1,32 @@
 const path = require('path');
 module.exports = {
-  module: {
-    rules: [
-      {
-        test: require.resolve("./geo"),
-        loader: "expose-loader",
-        options: {
-          exposes: ["geoapi", "./geo"],
-        },
-      },
-      {
-        test: require.resolve("./geo-util"),
-        loader: "expose-loader",
-        options: {
-          exposes: ["geoutil", "./geo-util"],
-        },
-      }
-    ]
-  },
   mode: process.env.NODE_ENV === 'development' ? 'development' : 'production',
-  entry: './index.js',
+  entry: {
+    index: './index.ts',
+    geo: './geo.ts',
+    'geo-util': './geo-util.ts',
+    viewport: './viewport.ts',    
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'], // resolve these extensions
+  },
   output: {
-    filename: 'geojsf.js',
-    path: path.resolve(__dirname, 'dist/')
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, './dist/')
     },
-  devtool: 'source-map',
-  optimization: {
-    minimize: false
-  }
+
+    module: {
+      rules: [
+        {
+          test: /\.tsx?$/, // regex to select only .ts or .tsx files
+          use: 'ts-loader', // use ts-loader to transpile TypeScript code to JavaScript
+          exclude: /node_modules/, // exclude node_modules directory
+        }
+      ]
+    },
+   
+    devtool: 'source-map',
+    optimization: {
+      minimize: false
+    }
 };
